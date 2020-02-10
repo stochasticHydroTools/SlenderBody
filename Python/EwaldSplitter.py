08608b1a665d49ca11bfb1ea128994eea174b455
Ondrej Maxian <om759@box743.cims.nyu.edu>
2020-02-10 16:55:56 -0500

    Third draft PYTHON code (not optimized)

--- Python/EwaldSplitter.py
+++ Python/EwaldSplitter.py
@@ -1,7 +1,5 @@
 import finufftpy as fi
 import numpy as np
-from scipy import special
-from scipy.spatial import cKDTree
 import EwaldUtils as ewc
 from math import pi
 
@@ -13,7 +11,9 @@ class EwaldSplitter(object):
     This class handles the Ewald splitting for the calculation of
     the non-local velocity.
     """
-    def __init__(self,Dom,xi,a,mu):
+    
+    ## METHODS FOR INITIALIZATION
+    def __init__(self,xi,a,mu):
         """
         Constructor. Initialize the Ewald splitter. 
         Input variables: Dom = domain object on which the Ewald is being done (this is the
@@ -24,46 +24,11 @@ class EwaldSplitter(object):
         self._xi = xi;
         self._a = a;
         self._mu = mu;
-        self._Dom = Dom;
         # Calculate the truncation distance for Ewald
-        self.calcrcut();
+        self._calcrcut();
 
-    def calcrcut(self):
-        """
-        Calculate the truncation distance for the Ewald near field. 
-        We truncate the near field at the value nearcut.
-        """
-        rcut=0;          # determine rcut
-        rcutstep = 1e-2; # accuracy of rcut
-        Vatcut = abs(np.array(ewc.RPYNKer([rcut,0,0],[1,0,0],self._mu,self._xi,self._a)));
-        V0 = min(np.amax(Vatcut),1); # M0 is relative to the value at 0 or 1, whichever is smaller
-        while (np.amax(Vatcut)/V0 > nearcut):
-            rcut=rcut+rcutstep;
-            Vatcut = abs(np.array(ewc.RPYNKer([rcut,0,0],[1,0,0],self._mu,self._xi,self._a)));
-        self._rcut =  rcut;
-        print 'Ewald cut %f' %self._rcut
-    
-    def checkrcut(self):
-        """
-        Check if rcut is less than one half period dynamically (the absolute
-        length of a half period varies as the strain g changes). 
-        If rcut is less than a half period, increase xi until rcut is less than a half
-        period.
-        """
-        Lmin = self._Dom.getminLen()/self._Dom.safetyfactor();
-        #print 'Safety distance is %f' %(Lmin*0.5)
-        vLover2 = np.amax(ewc.RPYNKer([Lmin*0.5,0,0],[1,0,0],self._mu,self._xi,self._a));
-        if (vLover2 > nearcut):
-            print 'Need to increase xi or L, there are near interactions w/ more than 1 image';
-        while (vLover2 > nearcut):
-            # Modify xi
-            self._xi+=0.1;
-            self.calcrcut();
-            vLover2 = np.amax(ewc.RPYNKer([Lmin*0.5,0,0],[1,0,0],self._mu,self._xi,self._a));
-            print 'The new value of xi is %f' %self._xi
-            print 'The new rcut %f' %self._rcut
-
-    def EwaldTotalVel(self,Npts,ptsxyz,forces,treeofPoints):
+    ## "PUBLIC" METHODS (NEEDED EXTERNALLY)
+    def EwaldTotalVel(self,Npts,ptsxyz,forces,Dom,treeofPoints):
         """
         Total velocity due to Ewald (far field + near field). 
         Inputs: Npts = the number of blobs, ptsxyz = the list of points 
@@ -72,15 +37,16 @@ class EwaldSplitter(object):
         Output: the total velocity as an Npts x 3 array.
         """
         # First check if Ewald parameter is ok
-        self.checkrcut();
-        Ewaldfar = self.EwaldFarVel(Npts,ptsxyz,forces); # far field Ewald
-        Ewaldnear = self.EwaldNearVelkD(Npts,ptsxyz,treeofPoints,forces); # near field Ewald
-        #Ewaldnear2 = self.EwaldNearVelQuad(Npts,ptsxyz,forces); # near field Ewald quadratic
+        self._checkrcut(Dom);
+        Ewaldfar = self._EwaldFarVel(Npts,ptsxyz,forces,Dom); # far field Ewald
+        Ewaldnear = self._EwaldNearVelkD(Npts,ptsxyz,Dom,treeofPoints,forces); # near field Ewald
+        #Ewaldnear2 = self._EwaldNearVelQuad(Npts,ptsxyz,forces,Dom); # near field Ewald quadratic
         #print 'Near field error'
         #print np.amax(Ewaldnear-Ewaldnear2)
-        return Ewaldfar+Ewaldnear;
+        return Ewaldfar+Ewaldnear; 
 
-    def EwaldFarVel(self,Npts,ptsxyz,forces):
+    ## "PRIVATE" METHODS (ONLY CALLED WITHIN THE CLASS)
+    def _EwaldFarVel(self,Npts,ptsxyz,forces,Dom):
         """
         This function computes the far field Ewald velocity. 
         Inputs: Npts = the number of blobs, ptsxyz = the list of points 
@@ -89,10 +55,10 @@ class EwaldSplitter(object):
         there for more information.
         """
         # Compute the coordinates in the transformed basis
-        pts = self._Dom.primecoords(ptsxyz);
-        g = self._Dom.getg();
+        pts = Dom.primecoords(ptsxyz);
+        g = Dom.getg();
         # Rescale to [-pi,pi] (for FINUFFT)
-        Lens = self._Dom.getLens();
+        Lens = Dom.getLens();
         pts = 2*pi*np.mod(pts,Lens)/Lens-pi;
         # Estimate the required grid
         gw=1.0/(2*self._xi);
@@ -138,14 +104,13 @@ class EwaldSplitter(object):
         fi.nufft3d2(pts[:,0],pts[:,1],pts[:,2],ux,1,fartol,uprojx,modeord=1);
         fi.nufft3d2(pts[:,0],pts[:,1],pts[:,2],uy,1,fartol,uprojy,modeord=1);
         fi.nufft3d2(pts[:,0],pts[:,1],pts[:,2],uz,1,fartol,uprojz,modeord=1);
-        vol = self._Dom.getVol();
+        vol = Dom.getVol();
         ux = np.real(ux)/vol;
         uy = np.real(uy)/vol;
         uz = np.real(uz)/vol;
         return np.concatenate(([ux],[uy],[uz])).T;
     
-    
-    def EwaldNearVelkD(self,Npts,ptsxyz,treeOfPoints,forces):
+    def _EwaldNearVelkD(self,Npts,ptsxyz,Dom,treeOfPoints,forces):
         """
         Near field velocity. 
         Inputs: Npts = the number of blobs, ptsxyz = the list of points 
@@ -157,21 +122,56 @@ class EwaldSplitter(object):
         # Find all pairs (returns an array of the pairs) within rcut
         #import time
         #t=time.time();
-        # print 'Doing Ewald with g=%f' %self._Dom.getg();
-        pairpts = self._Dom.kDTreeNeighbors(treeOfPoints, self._rcut);
+        # print 'Doing Ewald with g=%f' %Dom.getg();
+        pairpts = Dom.kDTreeNeighbors(treeOfPoints, self._rcut);
         Npairs = len(pairpts);
         #print 'Time to find pairs %f' %(time.time()-t);
         # Call the C+ function which takes as input the pairs of points, number of points and gives
         # you the near field
-        Lens = self._Dom.getLens();
-        g = self._Dom.getg();
+        Lens = Dom.getLens();
+        g = Dom.getg();
         velNear = ewc.RPYNKerPairs(Npts,Npairs,pairpts[:,0],pairpts[:,1],ptsxyz[:,0],ptsxyz[:,1],ptsxyz[:,2],\
                 forces[:,0],forces[:,1],forces[:,2],self._mu,self._xi,self._a,Lens[0],Lens[1],Lens[2],\
                 g, self._rcut);
-        return np.reshape(velNear,(Npts,3));
+        return np.reshape(velNear,(Npts,3));   
+
+    def _calcrcut(self):
+        """
+        Calculate the truncation distance for the Ewald near field. 
+        We truncate the near field at the value nearcut.
+        """
+        rcut=0;          # determine rcut
+        rcutstep = 1e-2; # accuracy of rcut
+        Vatcut = abs(np.array(ewc.RPYNKer([rcut,0,0],[1,0,0],self._mu,self._xi,self._a)));
+        V0 = min(np.amax(Vatcut),1); # M0 is relative to the value at 0 or 1, whichever is smaller
+        while (np.amax(Vatcut)/V0 > nearcut):
+            rcut=rcut+rcutstep;
+            Vatcut = abs(np.array(ewc.RPYNKer([rcut,0,0],[1,0,0],self._mu,self._xi,self._a)));
+        self._rcut =  rcut;
+        print 'Ewald cut %f' %self._rcut
     
-    
-    def EwaldNearVelQuad(self,Npts,pts,forces):
+    def _checkrcut(self, Dom):
+        """
+        Check if rcut is less than one half period dynamically (the absolute
+        length of a half period varies as the strain g changes). 
+        If rcut is less than a half period, increase xi until rcut is less than a half
+        period.
+        """
+        Lmin = Dom.getminLen()/Dom.safetyfactor();
+        #print 'Safety distance is %f' %(Lmin*0.5)
+        vLover2 = np.amax(ewc.RPYNKer([Lmin*0.5,0,0],[1,0,0],self._mu,self._xi,self._a));
+        if (vLover2 > nearcut):
+            print 'Need to increase xi or L, there are near interactions w/ more than 1 image';
+        while (vLover2 > nearcut):
+            # Modify xi
+            self._xi+=0.1;
+            self._calcrcut();
+            vLover2 = np.amax(ewc.RPYNKer([Lmin*0.5,0,0],[1,0,0],self._mu,self._xi,self._a));
+            print 'The new value of xi is %f' %self._xi
+            print 'The new rcut %f' %self._rcut
+
+    ## SLOW METHODS FOR CHECKING FAST METHODS
+    def _EwaldNearVelQuad(self,Npts,pts,forces,Dom):
         """
         Near field velocity. 
         Inputs: Npts = the number of blobs, ptsxyz = the list of points 
@@ -184,14 +184,14 @@ class EwaldSplitter(object):
         for iPt in xrange(Npts): # loop over points
             for jPt in xrange(Npts):
                 # Find nearest periodic image (might need to speed this up)
-                rvec = self._Dom.calcShifted(pts[iPt,:]-pts[jPt,:]);
+                rvec = Dom.calcShifted(pts[iPt,:]-pts[jPt,:]);
                 # Only actually do the computation when necessary
                 if (rvec[0]*rvec[0]+rvec[1]*rvec[1]+\
                     rvec[2]*rvec[2] < self._rcut*self._rcut):
                     velNear[:,iPt]+=ewc.RPYNKer(rvec,forces[jPt,:],self._mu,self._xi,self._a);
         return velNear.T;
     
-    def EwaldNearVelBins(self,Npts,pts,forces):
+    def _EwaldNearVelBins(self,Npts,pts,forces,Dom):
         """
         Near field velocity. 
         Inputs: Npts = the number of blobs, ptsxyz = the list of points 
@@ -201,8 +201,8 @@ class EwaldSplitter(object):
         print 'Warning - this near field function is based on bins and is SLOW!'
         velNear = np.zeros((pts.T).shape);
         # Sort points into bins
-        nxBin, nyBin, nzBin = self._Dom.calcnBins(self._rcut);
-        ptbins = self._Dom.binsbyP(pts,nxBin,nyBin,nzBin);
+        nxBin, nyBin, nzBin = Dom.calcnBins(self._rcut);
+        ptbins = Dom.binsbyP(pts,nxBin,nyBin,nzBin);
         print 'Number of bins (%d, %d, %d)' %(nxBin, nyBin, nzBin);
         bfirst, pnext = Dom.binPoints(Npts,pts,nxBin,nyBin,nzBin);
         # Go point by point to compute the velocity. Can parallelize this
@@ -224,17 +224,15 @@ class EwaldSplitter(object):
 
     # Python versions of functions - not being used as these functions are being
     # done in C++
-    
-    """
     def RPYNear(self,rvec):
-        ###
+        """
         #Evaluate the near field RPY kernel.
         #Input: the vector rvec to evaluate the kernel at
         #Output: value of kernel.
         #This function relies on the C++ functions Fnear
         #and Gnear, which evaluate the complicated near field \
         #kernels faster than Python.
-        ###
+        """
         rvec = np.reshape(rvec,(1,3));
         r=np.sqrt(rvec[:,0]*rvec[:,0]+rvec[:,1]*rvec[:,1]+rvec[:,2]*rvec[:,2]);
         rhat=rvec/r;
@@ -303,7 +301,6 @@ class EwaldSplitter(object):
             g3*np.exp(-(r+2*a)**2*xi**2)+g4*special.erfc(r*xi)+g5*special.erfc((r-2*a)*xi)+\
             g6*special.erfc((r+2*a)*xi);
         return val;
-    """
         
 
 
