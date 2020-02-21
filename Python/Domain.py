be3d8168efaa7f6ceaa14e11b3b9871515eeed99
Ondrej Maxian <om759@box743.cims.nyu.edu>
2020-02-21 09:57:03 -0500

    OOP updates

--- Python/Domain.py
+++ Python/Domain.py
@@ -1,27 +1,11 @@
 import numpy as np
-from scipy.spatial import cKDTree
 import EwaldUtils as ewc
 from math import sqrt
 
-# Donev: It seems to me this class exposes the KD tree underneath it
-# But it also contains functions at the end that use binning,not used right now
-# But we want to be able to have modular and extensible code so that
-# someday we may switch to another method for finding neighbors
-# So instead of calling something a initKDTree you want to call it
-# initSpatialDatabase
-# where Database is some generic object. I don't know
-# how scipy.spatial is designed but a good design would have an abstract class
-# for something that find neighbors, so one can switch different methods
-# and not just kDTrees. That is, is a kdTree a child class of some other more general
-# class, which can serve as a spatialDatabase?
-# It is OK to keep this as is as it takes some amount of work to overwrite and is not crucial
-# but just want to keep pointing these hard-wired choices to you so you are aware
-# What makes your job even harder here is that you need to be able to have multiple
-# spatial databases, not just one, so the callers need to be able to declare/pass
-# the spatial database themselves (otherwise you have to keep a database/list of databases, a pain)
 class Domain(object):
     """
-    This class handles the domain and the periodic BCs
+    This class handles the domain and the relevant BCs
+    (periodic domain is a child of general domain)
     """
     def __init__(self,Lx,Ly,Lz):
         """
@@ -58,169 +42,116 @@ class Domain(object):
     
     def getVol(self):
         return 1.0*self._Lx*self._Ly*self._Lz;
-    
-    def makekDTree(self,points):
-        """
-        Make a kD tree. 
-        Inputs: list of points to make the tree on as an N x 3 array. 
-        Return: the tree
-        """
-        ptsprime = self.primecoords(points);
-        # Mod the points so they are on the right bounds [0,Lx] x [0,Ly] x [0,Lz]
-        ptsprime = np.mod(ptsprime,self._Lens);
-        # Establish KD tree
-        return cKDTree(ptsprime,boxsize=self._Lens);
-    
-    def kDTreeNeighbors(self,pointTree,rcut):
-        """
-        Get the neighbors within an Eulerian distance rcut within the
-        ckD tree pointTree. 
-        Inputs: cKdTree pointTree, distance rcut
-        Output: pairs of neighbors as an nPairs x 2 array
-        """
-        rwsafety=rcut*self.safetyfactor(); # add the safety factor
-        return pointTree.query_pairs(rwsafety,output_type='ndarray');
-    
-    def TwokDTreesNeighbors(self,pointTree,NeighborTree,rcut):
-        """
-        Return a list of neighbors on the ckDtree NeighborTree that are 
-        within a distance rcut from the points on ckDTree PointTree.
-        Inputs: cKD tree of the points pointTree, neighbors NeighborTree, 
-        distance rcut
-        Outputs: nPoints list of neighbors for each point
-        """
-        rwsafety=rcut*self.safetyfactor(); # add the safety factor
-        return pointTree.query_ball_tree(NeighborTree,rwsafety);
+
+    def isPeriodic(self):
+        return 0;
     
     def calcShifted(self,dvec):
         """
-        Method to compute the nearest periodic image
-        of a point. The periodicity is in (x',y',z') 
-        where the strain in the coordinate system is given
-        by g. 
-        Inputs: vector (not shifted), and g
-        Output: vector shifted so it's on [-L/2,L/2]^3 (this is 
-        necessary to estimate the minimum norm)
+        Method to compute the nearest PERIODIC image
+        of a point/vector in UNDEFORMED coordinates.
+        Inputs: dvec is a 3 vector in (x,y,z)
+        undeformed coordinates
+        For a general free space domain, do nothing.
         """
-        # Just call the C++ function (the python was unbelievably slow!)
-        newvec = ewc.calcShifted(dvec, self._g, self._Lx, self._Ly, self._Lz);
-	# Donev: Instead of commenting out code like this, put this in an if-else statement
-	# That way one can switch and maybe compare they give the same answer etc.
-        #shift = 0*dvec;
-        ## Shift in oblique y and z
-        #shift[1] = round(dvec[1]/(1.0*self._Ly));
-        #shift[2] = round(dvec[2]/(1.0*self._Lz));
-        # Shift the vector
-        #dvec[0]-=g*self._Ly*shift[1];
-        #dvec[1]-=self._Ly*shift[1];
-        #dvec[2]-=self._Lz*shift[2];
-        # Shift in x
-        #shift[0] = round(dvec[0]/self._Lx);
-        #dvec[0]-=shift[0]*self._Lx;
-        #print dvec - cppres
-        return np.array(newvec);
+        return dvec;
+
+    def MinPrimeShiftInPrimeCoords(self,rprime):
+        """
+        Method to shift input DEFORMED coordiantes into
+        their minimum periodic image on [-L/2, L/2]. 
+        Input: rprime = N x 3 list of N vectors in deformed 
+        (x',y',z') coordinates. 
+        For a general domain, do nothing.
+        """
+        return rprime;
+
+    def ZeroLShiftInPrimeCoords(self,rprime):
+        """
+        Method to shift input DEFORMED coordiantes into
+        their minimum periodic image on [0,L].
+        Input: rprime = N x 3 list of N vectors in deformed 
+        (x',y',z') coordinates. 
+        For a general domain, check that the coordinates are in 
+        the right boundaries and then stop.
+        """
+        if (np.any(rprime > self._Lens) or np.any(rprime < 0) > 0):
+            raise ValueError('Points have gone out of the NON periodic domain');
+        return rprime;
 
     def primecoords(self,ptsxyz):
         """
         Method to calculate the coordinates of a vector 
         in the shifted coordinate system. Inputs = ptsxyz (pts in
-        the x,y,z coordinate system), and the strain g. 
+        the x,y,z coordinate system).
         Output is the vector pts', i.e. in the x',y',z' coordinate system
         """
-        L = np.array([[1,self._g,0],[0,1,0],[0,0,1]]);
-	# If this function is called often this seems wasteful
-	# I am sure you can just write a simple analytical result using Mathematica
-        pts = np.linalg.solve(L,ptsxyz.T).T;
-        return pts;
+        Linv = np.array([[1,-self._g,0],[0,1,0],[0,0,1]]);
+        ptsprime = np.dot(Linv,ptsxyz.T).T;
+        return ptsprime;
     
     def unprimecoords(self,ptsprime):
         """
         Method to compute the coordinates of a point in the (x,y,z)
         coordinate system from the coordinates in the (x',y',z') coordinate
-        system. Inputs: the prime coordinates and the strain g. 
+        system. Inputs: the prime coordinates.
         Outpts: the coordinates in the (x,y,z) space
         """
         L = np.array([[1,self._g,0],[0,1,0],[0,0,1]]);
         pts = np.dot(L,ptsprime.T).T;
         return pts;
 
-    #
-    # Stuff for binning the points (not used in the final version of the
-    # code, we are using kD trees instead)
-    # Donev: It would be curious to see at some point if this would run at descent speed with numba...
-    #
-    def calcnBins(self,rcut):
-        """
-        Calculate the number of bins in each direction for 
-        a given bin edge length rcut.
-        """
-        rwsafety=rcut*self.safetyfactor();
-        nx = int(float(self._Lx)/rwsafety);
-        ny = int(float(self._Ly)/rwsafety);
-        nz = int(float(self._Lz)/rwsafety);
-        return nx, ny, nz;
-    
-    def binPoints(self,Npts,pts,nxBin,nyBin,nzBin):
-        """ 
-        Bin the points.
-        Inputs = number of pts, locations of pts (in (x,y,z) space), 
-        nxBin, nyBin, nzBin = number of bins in each direction.
-        Returns the 2 linked lists first (for the first point in each bin) and
-        next (for the next point in the bin).
-        """
-        bins = self.binsbyP(pts,nxBin,nyBin,nzBin);
-        sbins = bins[:,0]+nxBin*bins[:,1]+nxBin*nyBin*bins[:,2];
-        # Form the linked lists
-        bfirst = -np.ones(nxBin*nyBin*nzBin,dtype=np.int);
-        pnext = -np.ones(Npts,dtype=np.int);
-        for iPt in range(Npts):
-            if (bfirst[sbins[iPt]] == -1):
-                bfirst[sbins[iPt]] = iPt;
-            else:
-                jPt = bfirst[sbins[iPt]];
-                while (jPt !=-1):
-                    jPtprev = jPt;
-                    jPt = pnext[jPt];
-                pnext[jPtprev] = iPt;
-        return bfirst, pnext;
-    
-    def binsbyP(self,pts,nxBin,nyBin,nzBin):
-        """
-        Get the bins for each point. 
-        Inputs: pts = Npts x 3 array of the pts in (x,y,z) space,
-        nxBin, nyBin, nzBin = number of bins in each direction. 
-        Return: the bin number for each point as an array
-        """
-        coords = self.primecoords(pts);
-        # Shift all coordinates so they are on [0,L]^3
-        coords = coords - np.floor(coords/self._Lens)*self._Lens;
-        # Get the bin for each pt
-        dperbin = np.array([[float(nxBin)/self._Lx, float(nyBin)/self._Ly,\
-                                float(nzBin)/self._Lz]]);
-        bins = np.int_(coords*dperbin);
-        bins = np.mod(bins,[nxBin, nyBin, nzBin]); # takes care of any rounding issues
-        return bins;
-    
-    @staticmethod
-    def neighborBins(tbin,nxBin,nyBin,nzBin):
-        """
-        Neighbor bins for each bin
-        Input: the bin as a 3 array (iBin,jBin,kBin)
-        Output: list of at most 27 (possibly less if there are less
-        than 3 bins in a direction) neighbor bins as a 27 array. 
-        """
-        neighbors = tbin+np.int_([[0,0,0],[1,0,0],[-1,0,0],[0,1,0],\
-                        [1,1,0],[-1,1,0],[0,-1,0],[1,-1,0],[-1,-1,0],[0,0,1],\
-                        [1,0,1],[-1,0,1],[0,1,1],[1,1,1],[-1,1,1],[0,-1,1],\
-                        [1,-1,1],[-1,-1,1],[0,0,-1],[1,0,-1],[-1,0,-1],\
-                        [0,1,-1,],[1,1,-1],[-1,1,-1],[0,-1,-1],[1,-1,-1],\
-                        [-1,-1,-1]]);
-        neighbors = np.mod(neighbors,[nxBin, nyBin, nzBin]);
-        _, idx = np.unique(neighbors,axis=0,return_index=True);
-        neighbors = neighbors[np.sort(idx)];
-        sN = neighbors[:,0]+nxBin*neighbors[:,1]+nxBin*nyBin*neighbors[:,2];
-        return sN;
-
+    def __del__(self):
+        # Destructor
+        print('Domain object destroyed');
         
 
+class PeriodicDomain(Domain):
+    
+    """
+    Child class of Domain that handles periodic BCs
+    """
+    
+    def __init__(self,Lx,Ly,Lz):
+        super().__init__(Lx,Ly,Lz);
+
+    def calcShifted(self,dvec):
+        """
+        Method to compute the nearest periodic image
+        of a point. The periodicity is in (x',y',z').
+        Inputs: vector (not shifted) dvec.
+        Output: vector shifted so it's on [-L/2,L/2]^3 (this is 
+        necessary to estimate the minimum norm)
+        """
+        # Just call the C++ function (the python was unbelievably slow!)
+        newvec = ewc.calcShifted(dvec, self._g, self._Lx, self._Ly, self._Lz);
+        if (False):
+            shift = 0*dvec;
+            ## Shift in oblique y and z
+            shift[1] = round(dvec[1]/(1.0*self._Ly));
+            shift[2] = round(dvec[2]/(1.0*self._Lz));
+            # Shift the vector
+            dvec[0]-=g*self._Ly*shift[1];
+            dvec[1]-=self._Ly*shift[1];
+            dvec[2]-=self._Lz*shift[2];
+            # Shift in x
+            shift[0] = round(dvec[0]/self._Lx);
+            dvec[0]-=shift[0]*self._Lx;
+        return np.array(newvec);
+
+    def MinPrimeShiftInPrimeCoords(self,rprime):
+        """
+        Shift input to [-L/2, L/2] (see super docstring)
+        """
+        rprime-= np.round(rprime/self._Lens)*self._Lens;
+        return rprime;
+
+    def ZeroLShiftInPrimeCoords(self,rprime):
+        """
+        Shift input to [0,L] (see super docstring)
+        """
+        return np.mod(rprime,self._Lens);
+
+    def isPeriodic(self):
+        return 1;
 
---
+++ Python/SpatialDatabase.py
@@ -0,0 +1,276 @@
+import numpy as np
+from scipy.spatial import cKDTree
+
+class SpatialDatabase(object):
+
+    """
+    Spatial database object. Its role is to compute neighbors
+    between sets of points.
+    """
+    
+    def __init__(self,pts,Dom):
+        """
+        Constructor. Inputs are the set of points and the Domain 
+        which are used to initialize the ptsprime variable
+        """
+        self._Npts,_ = pts.shape;
+        self.updateSpatialStructures(pts,Dom);
+
+    def updateSpatialStructures(self,pts,Dom):
+        """
+        Update all relevant spatial structures. In the general case, 
+        this is just updating the deformed coordinates of the points.
+        """
+        self._ptsprime = Dom.primecoords(pts);
+        
+    def selfNeighborList(self,rcut,Dom):
+        """
+        Compute a list of neighbors. Compute pairs
+        of pts (i,j) that are a distance r w/safety factor apart in 
+        the primed (deformed) norm.
+        General method is a quadratic loop
+        """
+        rwsafety=rcut*Dom.safetyfactor(); # add the safety factor
+        # Quadratic loop
+        neighbors = [];
+        for iPt in range(self._Npts):
+            for jPt in range(iPt+1,self._Npts):
+                rvecprime = self._ptsprime[iPt,:]-self._ptsprime[jPt,:];
+                # Shift rvec so it's on [-L/2, L/2]^3
+                rvecprime = Dom.MinPrimeShiftInPrimeCoords(rvecprime);
+                if (np.linalg.norm(rvecprime) < rwsafety):
+                    neighbors.append([iPt,jPt]);
+        return np.array(neighbors);
+            
+    def otherNeighborsList(self,other,rcut,Dom):
+        """
+        Compute a list of neighbors between 2 different point sets.
+        Pairs of pts (i,j) that are a distance r w/safety factor apart in
+        the primed norm, where i is in self._pts and j is in other._pts
+        General method is a quadratic loop
+        """
+        rwsafety=rcut*Dom.safetyfactor(); # add the safety factor
+        # Quadratic loop
+        neighbors = [];
+        for iPt in range(self._Npts):
+            iNeighbors=[];
+            for jPt in range(other._Npts):
+                rvecprime = self._ptsprime[iPt,:]-other._ptsprime[jPt,:];
+                # Shift rvec so it's on [-L/2, L/2]^3
+                rvecprime = Dom.MinPrimeShiftInPrimeCoords(rvecprime);
+                if (np.linalg.norm(rvecprime) < rwsafety):
+                    iNeighbors.append(jPt);
+            neighbors.append(iNeighbors);
+        return neighbors;
+
+    def __del__(self):
+        # Destructor
+        print('SpatialDatabase object destroyed');
+     
+
+class ckDSpatial(SpatialDatabase):
+
+    """
+    Child of SpatialDatabase that uses ckD trees to compute 
+    neighbors efficiently.
+    """
+    
+    def __init__(self,pts,Dom):
+        """
+        Constructor. Initialize the kD tree.
+        """
+        self.updateSpatialStructures(pts,Dom);
+    
+    def updateSpatialStructures(self,pts,Dom):
+        """
+        Update the kD tree using the set of points pts (an N x 3)
+        array, and Domain object Dom.
+        """
+        ptsprime = Dom.primecoords(pts);
+        # Mod the points so they are on the right bounds [0,Lx] x [0,Ly] x [0,Lz]
+        # (needed for the call to kD tree)
+        ptsprime = Dom.ZeroLShiftInPrimeCoords(ptsprime);
+        # The domain can be periodic or free space. If periodic, pass
+        # that information to the kD tree.
+        BoxLens = None;
+        if (Dom.isPeriodic()):
+            BoxLens = Dom.getLens();
+        # Update the KD tree
+        self._myKDTree = cKDTree(ptsprime,boxsize=BoxLens);
+
+    def selfNeighborList(self,rcut,Dom):
+        """
+        Get the neighbors within an Eulerian distance rcut (same 
+        as rcut*safety factor in the deformed norm) within the
+        ckD tree pointTree. 
+        Inputs: distance rcut, domain object Dom where this is happening
+        Output: pairs of neighbors as an nPairs x 2 array
+        """
+        rwsafety=rcut*Dom.safetyfactor(); # add the safety factor
+        neighbors = self._myKDTree.query_pairs(rwsafety,output_type='ndarray');
+        return neighbors;
+
+    def otherNeighborsList(self,other,rcut,Dom):
+        """
+        Return a list of neighbors on another ckDtree that are
+        within a distance rcut from the points on self._mkDTree.
+        Inputs: other ckDSpatial object with its kD tree, Eulerian 
+        distance rcut, Domain object Dom
+        Outputs: nPoints list of neighbors for each point
+        """
+        rwsafety=rcut*Dom.safetyfactor(); # add the safety factor
+        return self._myKDTree.query_ball_tree(other._myKDTree,rwsafety);
+
+class LinkedListSpatial(SpatialDatabase):
+
+    """
+    Child of SpatialDatabase that uses linked lists to do neighbor 
+    searches. 
+    """
+    
+    def __init__(self,pts,Dom):
+        super().__init__(pts,Dom);
+    
+    def selfNeighborList(self,rcut,Dom):
+        """
+        Get the neighbors within an Eulerian distance rcut using
+        the linked list construction. 
+        Inputs: rcut, Domain object
+        Output: pairs of neighbors as an nPairs x 2 array
+        """
+        rwsafety=rcut*Dom.safetyfactor(); # add the safety factor
+        neighbors = [];
+        nbins = self.calcnBins(rwsafety,Dom);
+        ptbins = self.binsbyP(nbins,Dom);
+        #print('Number of bins (%d, %d, %d)' %(nbins[0], nbins[1], nbins[2]));
+        # First and next linked lists
+        bfirst, pnext = self.binPoints(nbins,Dom);
+        # Loop over points. For each point, check neighboring bins
+        # for other points.
+        for iPt in range(self._Npts): # loop over points
+            tbin=[ptbins[iPt,:]];
+            neighBins = LinkedListSpatial.neighborBins(tbin,nbins);
+            for iSn in range(len(neighBins)): # loop over neighboring bins
+                jPt = bfirst[neighBins[iSn]];
+                while (jPt !=-1):
+                    rvecprime = self._ptsprime[iPt,:]-self._ptsprime[jPt,:];
+                    # Shift rvec so it's on [-L/2, L/2]^3
+                    rvecprime = Dom.MinPrimeShiftInPrimeCoords(rvecprime);
+                    # Only include points that are close enough (and include
+                    # each pair only once)
+                    if (jPt > iPt and np.linalg.norm(rvecprime) < rwsafety):
+                        neighbors.append([iPt,jPt]);
+                    jPt = pnext[jPt];
+        neighbors = np.array(neighbors);        
+        return neighbors;
+
+    def otherNeighborsList(self,other,rcut,Dom):
+        """
+        Get the neighbors of self within the object other
+        for an Eulerian distance rcut. Using linked lists, 
+        we iterate
+        Inputs: other set of points, rcut, Domain object
+        Output: Npts list of neighbors for each point
+        """
+        rwsafety=rcut*Dom.safetyfactor(); # add the safety factor
+        neighbors = [];
+        nbins = self.calcnBins(rwsafety,Dom);
+        # Bin the self points
+        ptbins = self.binsbyP(nbins,Dom);
+        print('Number of bins (%d, %d, %d)' %(nbins[0], nbins[1], nbins[2]));
+        # First and next linked lists for the OTHER points
+        bfirst, pnext = other.binPoints(nbins,Dom);
+        # Loop over points. For each point, check neighboring bins
+        # for points in other.
+        for iPt in range(self._Npts): # loop over points
+            iNeighbors=[];
+            tbin=[ptbins[iPt,:]];
+            neighBins = LinkedListSpatial.neighborBins(tbin,nbins);
+            for iSn in range(len(neighBins)): # loop over neighboring bins
+                jPt = bfirst[neighBins[iSn]]; # point in OTHER
+                while (jPt !=-1):
+                    rvecprime = self._ptsprime[iPt,:]-other._ptsprime[jPt,:];
+                    # Shift rvec so it's on [-L/2, L/2]^3
+                    rvecprime = Dom.MinPrimeShiftInPrimeCoords(rvecprime);
+                    # Only include points that are close enough
+                    if (np.linalg.norm(rvecprime) < rwsafety):
+                        iNeighbors.append(jPt);
+                    jPt = pnext[jPt];
+            neighbors.append(iNeighbors);
+        return neighbors;
+
+    def calcnBins(self,binlen,Dom):
+        """
+        Calculate the number of bins in each direction for 
+        a given bin edge length rcut.
+        """
+        nbins = np.int_(Dom.getLens()/binlen);
+        return nbins;
+    
+    def binsbyP(self,nbins,Dom):
+        """
+        Get the bins for each point. 
+        Inputs: nbins (3 array with number of bins in each direction), 
+        Domain object that computation is done on.
+        Return: the bin number for each point as an array as an N x 3 
+        array where the columns are the bin numbers in each direction.
+        """
+        # Shift all coordinates so they are on [0,L]^3 (assuming this
+        # will only be necessary for a periodic domain
+        coords = Dom.ZeroLShiftInPrimeCoords(self._ptsprime);
+        # Get the bin for each pt
+        dperbin = Dom.getLens()/nbins;
+        bins = np.int_(coords/dperbin);
+        bins = np.mod(bins,nbins); # takes care of any rounding issues
+        return bins;    
+
+    def binPoints(self,nbins,Dom):
+        """ 
+        Bin the points, i.e. create the linked lists first and next.
+        Inputs = nbins (3 array with number of bins in each direction), 
+        Domain object that computation is done on.
+        Returns the 2 linked lists first (for the first point in each bin) and
+        next (for the next point in the bin).
+        """
+        bins = self.binsbyP(nbins,Dom);
+        sbins = bins[:,0]+nbins[0]*bins[:,1]+nbins[0]*nbins[1]*bins[:,2];
+        # Form the linked lists
+        bfirst = -np.ones(nbins[0]*nbins[1]*nbins[2],dtype=np.int);
+        pnext = -np.ones(self._Npts,dtype=np.int);
+        for iPt in range(self._Npts):
+            if (bfirst[sbins[iPt]] == -1):
+                bfirst[sbins[iPt]] = iPt;
+            else:
+                jPt = bfirst[sbins[iPt]];
+                while (jPt !=-1):
+                    jPtprev = jPt;
+                    jPt = pnext[jPt];
+                pnext[jPtprev] = iPt;
+        return bfirst, pnext;
+    
+    @staticmethod
+    def neighborBins(tbin,nbins):
+        """
+        Neighbor bins for each bin
+        Input: the bin as a 3 array (iBin,jBin,kBin)
+        Output: list of at most 27 (possibly less if there are less
+        than 3 bins in a direction) neighbor bins as a 27 array. 
+        Note: it is ok to have periodic neighbors for a general free
+        space domain since those points will just get dropped anyway. 
+        """
+        neighbors = tbin+np.int_([[0,0,0],[1,0,0],[-1,0,0],[0,1,0],\
+                        [1,1,0],[-1,1,0],[0,-1,0],[1,-1,0],[-1,-1,0],[0,0,1],\
+                        [1,0,1],[-1,0,1],[0,1,1],[1,1,1],[-1,1,1],[0,-1,1],\
+                        [1,-1,1],[-1,-1,1],[0,0,-1],[1,0,-1],[-1,0,-1],\
+                        [0,1,-1,],[1,1,-1],[-1,1,-1],[0,-1,-1],[1,-1,-1],\
+                        [-1,-1,-1]]);
+        neighbors = np.mod(neighbors,nbins);
+        _, idx = np.unique(neighbors,axis=0,return_index=True);
+        neighbors = neighbors[np.sort(idx)];
+        sN = neighbors[:,0]+nbins[0]*neighbors[:,1]+nbins[0]*nbins[1]*neighbors[:,2];
+        return sN;
+        
+
+
+
+    
