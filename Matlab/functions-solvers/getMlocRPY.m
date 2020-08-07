67f7442b9f0ce39d720acc69a7877efb4e1d87ba
Ondrej Maxian <om759@access1.cims.nyu.edu>
2020-08-07 15:41:21 -0400

    Update with Aleks comments

--- Matlab/functions-solvers/getMlocRPY.m
+++ Matlab/functions-solvers/getMlocRPY.m
@@ -1,9 +1,24 @@
 % Get the mobility matrix from Xs
-function M = getMlocRPY(N,Xs,eps,Lf,mu,s0)
+function M = getMlocRPY(N,Xs,eps,Lf,mu,s0,delta)
+    as = zeros(length(s0),1);
+    for iS=1:length(s0)
+        s = s0(iS);
+        if (s < delta*Lf || s > Lf-delta*Lf)
+            as(iS) = eps*2*sqrt(s*(Lf-s)); % ellipsoidal tapering
+        elseif (s < 2*delta*Lf)
+            wCyl = 1/(1+exp(-23.0258/(delta*Lf)*s+34.5387));
+            as(iS) = eps*(Lf*wCyl+(1-wCyl)*2*sqrt(s*(Lf-s)));
+        elseif (s > Lf-2*delta*Lf)
+            wCyl = 1/(1+exp(-23.0258/(delta*Lf)*(Lf-s)+34.5387));
+            as(iS) = eps*(Lf*wCyl+(1-wCyl)*2*sqrt(s*(Lf-s)));
+        else
+            as(iS) = eps*Lf;
+        end
+    end
+    Ls = log(4.*s0.*(Lf-s0)./as.^2); 
     k = sqrt(3/2);
     aI = 47/12-log(16*k^2);
     atau = 1/4-log(16*k^2);
-    L = log(4.*s0.*(Lf-s0)./(eps*Lf)^2);      
     rows=zeros(9*N,1);
     cols=zeros(9*N,1);
     vals=zeros(9*N,1);
@@ -15,7 +30,7 @@ function M = getMlocRPY(N,Xs,eps,Lf,mu,s0)
             [inds(1) inds(1) inds(1) inds(2) inds(2) inds(2) inds(3) inds(3) inds(3)]';
         cols((iPt-1)*9+1:iPt*9)=...
             [inds(1) inds(2) inds(3) inds(1) inds(2) inds(3) inds(1) inds(2) inds(3)]';
-        Mloc=1/(8*pi*mu)*((aI*eye(3)+atau*XsXs)+L(iPt)*(eye(3)+XsXs));
+        Mloc=1/(8*pi*mu)*((aI*eye(3)+atau*XsXs)+Ls(iPt)*(eye(3)+XsXs));
         vals((iPt-1)*9+1:iPt*9)=Mloc(:);
     end
     M = sparse(rows,cols,vals);
