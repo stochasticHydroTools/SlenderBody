d24cdabba9ea73a3dc8d136cec9e75f82035a9cc
Ondrej Maxian <om759@access2.cims.nyu.edu>
2020-08-08 12:16:49 -0400

    Simpson for KT lambda

--- Matlab/ChebyshevAndBlobs/KstlambdaChebMB.m
+++ Matlab/ChebyshevAndBlobs/KstlambdaChebMB.m
@@ -21,7 +21,7 @@ lamStack=reshape(lambda',3*NCheb,1);
 Ktlam = [Kt*lamStack; (w*lambda)'];
 
 %% Uniform points version
-Nuni = 25;
+Nuni = 50;
 ds = 2/(Nuni-1);
 sUni=(-1:ds:1)';
 % Matrix that takes coefficients of Cheb polynomials to values on the
@@ -38,31 +38,39 @@ theta(abs((abs(phi)-pi/2)) < 1e-12) =0;
 n1s=[-sin(theta) cos(theta) 0*theta];
 n2s=[-cos(theta).*sin(phi) -sin(theta).*sin(phi) cos(phi)];
 % Evaluate K^* lambda for Brennan (stacking it differently from the notes)
-KstBlob = ds*makeKstBlob(lamUni,n1s,n2s,Nuni);
-KstCheb_fromBlob = convertKstBlob(KstBlob,CoeffsToValsUniform,NCheb-1,Nuni,ds);
+KstBlob = makeKstBlob(lamUni,n1s,n2s,sUni);
+KstCheb_fromBlob = convertKstBlob(KstBlob,CoeffsToValsUniform,NCheb-1,sUni);
 max(abs(Ktlam-KstCheb_fromBlob))
 
-function KstBlob = makeKstBlob(lams,n1s,n2s,Nuni)
+function KstBlob = makeKstBlob(lams,n1s,n2s,sUni)
     % Evaluates Brennan's formula for the blobs
     % K^T lambda = [n_1(s) dot integral_s^L lambda(s)] for each s
+    Nuni = length(sUni);
     KstBlob = zeros(2*Nuni+3,1);
     for iPt=1:Nuni-1
-        lamInt = sum(lams(iPt:end,:))-0.5*(lams(iPt,:)+lams(end,:));
+        if (iPt < Nuni-1) % These ones have at least 3 points
+            lamInt =  simpson_nonuniform(sUni(iPt:end), lams(iPt:end,:));
+        else % Only 2 points, do trapezoid
+            lamInt = (lams(end,:)+lams(end-1,:))*(sUni(end)-sUni(end-1))/2;
+        end
         KstBlob(iPt)=dot(n1s(iPt,:),lamInt);
         KstBlob(iPt+Nuni)=dot(n2s(iPt,:),lamInt);
     end
-    KstBlob(2*Nuni+1:2*Nuni+3)=sum(lams)-0.5*(lams(1,:)+lams(end,:)); % add integrals of lambda
+    % add integrals of lambda
+    KstBlob(2*Nuni+1:2*Nuni+3)=simpson_nonuniform(sUni, lams); 
 end
 
-function KstCheb = convertKstBlob(KstBlob,CoeffsToValsUniform,Npolys,Nuni,ds)
+
+function KstCheb = convertKstBlob(KstBlob,CoeffsToValsUniform,Npolys,sUni)
     % This is the function I wrote on paper to convert the multiblob to
     % Chebyshev.
     KstCheb = zeros(2*Npolys+3,1);
+    Nuni = length(sUni);
     for iPoly=1:Npolys
         n1prods = KstBlob(1:Nuni).*CoeffsToValsUniform(:,iPoly);
         n2prods = KstBlob(Nuni+1:2*Nuni).*CoeffsToValsUniform(:,iPoly);
-        KstCheb(iPoly)=ds*(sum(n1prods)-0.5*(n1prods(1,:)+n1prods(end,:)));
-        KstCheb(Npolys+iPoly)=ds*(sum(n2prods)-0.5*(n2prods(1,:)+n2prods(end,:)));
+        KstCheb(iPoly)=simpson_nonuniform(sUni,n1prods);
+        KstCheb(Npolys+iPoly)=simpson_nonuniform(sUni,n2prods);
     end
     KstCheb(2*Npolys+1:2*Npolys+3)=KstBlob(2*Nuni+1:2*Nuni+3);
 end
