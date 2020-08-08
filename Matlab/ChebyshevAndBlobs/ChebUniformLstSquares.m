26e522599cd8d74a006d90b660f7fda1e37e6d42
Ondrej Maxian <om759@access2.cims.nyu.edu>
2020-08-08 11:19:59 -0400

    L2 projection for uniform to Cheb

--- Matlab/ChebyshevAndBlobs/ChebUniformLstSquares.m
+++ Matlab/ChebyshevAndBlobs/ChebUniformLstSquares.m
@@ -1,61 +1,74 @@
-close all;
+% Least squares in L^2 to obtain Chebyshev coefficients of a function on a
+% uniform grid
+
+% Build resampling matrices
 % Chebyshev coefficients to Chebyshev values
 NCheb = 16;
 th_Cheb =fliplr(2.0*(0:NCheb-1)'+1)*pi/(2*NCheb);
 sCheb = cos(th_Cheb);
 CoeffsToValsCheb = cos(th_Cheb.*(0:NCheb-1));
 % Chebyshev coefficients to uniform values
-Nuni = 100;
-sUni = (1:-2/(Nuni-1):-1)';
+Nuni = 10000;
+ds = 2/(Nuni-1);
+sUni = (1:-ds:-1)';
 CoeffstoValsUniform = cos(acos(sUni).* (0:NCheb-1));
 % Matrix from Cheb values to uniform values
 ChebtoUniform = CoeffstoValsUniform*CoeffsToValsCheb^-1;
 % Check
 % y = sCheb.^5-sCheb.^10+sCheb;
-% max(abs(ChebtoUniform*y-(sUni.^5-sUni.^10+sUni)));
-% Take 16 randomly decaying Chebyshev coefficients
+% max(abs(ChebtoUniform*y-(sUni.^5-sUni.^10+sUni)))
+% Take 16 randomly decaying Chebyshev coefficients and sample that
+% Chebyshev series on the uniform grid
 rng(0);
-chat = rand(NCheb,1).*exp(-0.1*(0:NCheb-1)');
+chat = rand(NCheb,1).*exp(-0.5*(0:NCheb-1)');
 univals = CoeffstoValsUniform*chat; % values on uniform grid
-% Use backslash to get coefficients
-chat2=CoeffstoValsUniform \ univals;
-max(abs(chat-chat2))
-% Try to get 24 coefficients
-CoeffstoValsUniform24 = cos(acos(sUni).* (0:24-1));
-chat24 = CoeffstoValsUniform24 \ univals;
-max(abs(chat-chat24(1:NCheb))) % answer is the same!
-% Now add some random noise on the uniform grid
-univalsPerturbed = univals+rand(Nuni,1)*0.1;
-plot(sUni,univals,sUni,univalsPerturbed,'LineWidth',2.0)
-legend('Original function','With noise')
-% Try to get 16 coefficients
-chatpert=CoeffstoValsUniform \ univalsPerturbed;
-% Try to get 24 coefficients
-chatpert24 = CoeffstoValsUniform24 \ univalsPerturbed;
-% Plot the spectrum
-figure;
-semilogy(0:NCheb-1,abs(chat),'-o',0:NCheb-1,abs(chatpert),'--s',0:24-1,abs(chatpert24),'-.d',...
-    'LineWidth',2.0,'MarkerSize',8)
-legend('Original spectrum','Perturbed spectrum (16 coeffs)','Perturbed spectrum (24 coeffs)')
+univalsPerturbed = univals+(rand(Nuni,1)-0.5)*0.1; % randomly perturbed
+plot(sUni,univalsPerturbed)
+hold on
+plot(sUni,univals,'LineWidth',2.0)
+legend('Perturbed data','Original series')
 
-% Alternative way with polyfit (equivalent to the above)
-% p = polyfit(sUni,univalsPerturbed,NCheb-1);
-% ChebVals_polyfit = polyval(p,sCheb);
-% ChebCoeffs_polyfit = CoeffsToValsCheb \ ChebVals_polyfit;
-% max(abs(ChebCoeffs_polyfit-chat))
-
-% Compute coefficients by integration (equally spaced in theta)
+% Compute coefficients by integration (Simpson's rule for theta)
 % Weights for trapezoidal rule
 thetasUni = acos(sUni);
-dtheta = thetasUni(2:end)-thetasUni(1:end-1);
-wtsTheta = [dtheta(1)/2; (dtheta(1:end-1)+dtheta(2:end))/2; dtheta(end)/2];
-% Integrals are function values * wts * matrix of Chebyshev values
-ChebInts = CoeffstoValsUniform'*(univals.*wtsTheta);
+dthetaMax = acos(1-ds)-acos(1); % for convergence tracking
+% Simpson's rule to compute integrals
+ChebIntsSimp=zeros(NCheb,1);
+ChebIntsSimpPert=zeros(NCheb,1);
+for k=1:NCheb
+    fk = CoeffstoValsUniform(:,k).*univals;
+    fpertk = CoeffstoValsUniform(:,k).*univalsPerturbed;
+    ChebIntsSimp(k) = simpson_nonuniform(acos(sUni),fk);
+    ChebIntsSimpPert(k) = simpson_nonuniform(acos(sUni),fpertk);
+end
 % Normalize by Chebyshev integrals 
-CoeffsFromInt = ChebInts./[pi; pi/2*ones(NCheb-1,1)];
-% These are the non perturbed ones
-% You need LOTS of points to get the same accuracy because this is a
-% second-order scheme
-% Perturbed ones
-ChebIntsPert = CoeffstoValsUniform'*(univalsPerturbed.*wtsTheta);
-CoeffsPertFromInt = ChebIntsPert./[pi; pi/2*ones(NCheb-1,1)];
+ChebIntsSimp = ChebIntsSimp./[pi; pi/2*ones(NCheb-1,1)];
+ChebIntsSimpPert = ChebIntsSimpPert./[pi; pi/2*ones(NCheb-1,1)];
+% Error 
+max(abs(ChebIntsSimp-chat))
+% Plot normal and perturbed coefficients
+figure;
+semilogy(0:NCheb-1,abs(chat),'-o',0:NCheb-1,abs(ChebIntsSimp),'s',0:NCheb-1,abs(ChebIntsSimpPert),'d',...
+    'MarkerSize',8,'LineWidth',2.0)
+legend('Original','Simpson','Simpson for perturbed')
+
+function result = simpson_nonuniform(x, f)
+    %Simpson rule for irregularly spaced data.
+    N = length(x) - 1;
+    h = x(2:end)-x(1:end-1);
+    result = 0.0;
+    for i=2:2:N
+        hph = h(i) + h(i - 1);
+        result=result+ f(i) * ( h(i).^3 + h(i - 1).^3+ 3. * h(i) * h(i - 1) * hph )...
+                     / ( 6 * h(i) * h(i - 1));
+        result =result+ f(i - 1) * ( 2. * h(i - 1).^3 - h(i).^3+ 3. * h(i) * h(i - 1).^2)...
+                     / ( 6 * h(i - 1) * hph);
+        result =result+ f(i + 1) * ( 2. * h(i).^3 - h(i - 1).^3 + 3. * h(i - 1) * h(i).^2)...
+                     / ( 6 * h(i) * hph );
+    end
+    if (mod((N + 1),2) == 0)
+        result =result+ f(N+1) * ( 2 * h(N).^2 + 3. * h(N-1) * h(N))/ ( 6 *( h(N-1) + h(N)));
+        result =result+ f(N)*(h(N).^2+ 3*h(N)* h(N-1))/( 6 * h(N-1));
+        result =result- f(N-1)* h(N).^3/( 6 * h(N-1) * (h(N-1) + h(N)));
+    end
+end
