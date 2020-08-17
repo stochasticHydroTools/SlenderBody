c1243a8a8a833a9b0052b16b42cc77db4f47e4da
Ondrej Maxian <om759@box747.cims.nyu.edu>
2020-08-17 15:11:15 -0400

    Rename files

--- Matlab/functions-solvers/getMlocRPY.m
+++ Matlab/functions-solvers/getMlocRPY.m
@@ -1,37 +1,27 @@
 % Get the mobility matrix from Xs
-function M = getMlocRPY(N,Xs,eps,Lf,mu,s0,delta)
-%     Ls = log(4.*s0.*(Lf-s0)./(eps*Lf).^2); 
-    % Regularize L over lengthscale delta
-%     for iS=1:length(s0)
-%         s = s0(iS);
-%         if (s/Lf < 0.5)
-%             wCyl = 1/(1+exp(-36.84/Lf*s+9.2102)); % 10^-3 at 0 and at center
-%             Ls(iS) = wCyl*Ls(iS)+(1-wCyl)*log(4*delta*(1-delta)./eps^2);
-%         elseif (s/Lf > 0.5)
-%             wCyl = 1/(1+exp(-36.84/Lf*(Lf-s)+9.2102));
-%             Ls(iS) = wCyl*Ls(iS)+(1-wCyl)*log(4*delta*(1-delta)./eps^2);   
-%         end
-%     end
-    x = 2*s0/Lf-1;
-    Lnormal = 4.*s0.*(Lf-s0)./(eps*Lf).^2;
-    regwt = tanh((x+1)/delta)-tanh((x-1)/delta)-1;
-    Ldelta = 4*delta/2*(1-delta/2)/eps^2;
-    Ls = log(regwt.*Lnormal+(1-regwt.^2).*Ldelta);
-    aI = 1;
-    atau = -3;
-    rows=zeros(9*N,1);
-    cols=zeros(9*N,1);
-    vals=zeros(9*N,1);
+function M = getMlocRPYUnreg(N,Xs,a,L,mu,s0)
+    % Regularized L
+    aI = zeros(N,1);
+    atau = zeros(N,1);
+    for iT=1:N
+        t = s0(iT);
+        if (t > 2*a && t < L-2*a)
+            aI(iT) = log((4*(L-t).*t)/(a^2))-log(16)+(1/6-2/3*(a^2/(2*t^2)+a^2/(2*(L-t)^2)))+23/6;
+            atau(iT) = log((4*(L-t).*t)/(a^2))-log(16)-3*(1/6-2/3*(a^2/(2*t^2)+a^2/(2*(L-t)^2)))+1/2;
+        elseif (t <=2*a)
+            aI(iT) = log((L-t)/(2*a))+2+4*t/(3*a)-3*t^2/(16*a^2);
+            atau(iT) = log((L-t)/(2*a))+t^2/(16*a^2);
+        else
+            aI(iT) = log(t/(2*a))+2+4*(L-t)/(3*a)-3*(L-t)^2/(16*a^2);
+            atau(iT) = log(t/(2*a))+(L-t)^2/(16*a^2);
+        end
+    end
+    M = zeros(3,3,N);
     for iPt=1:N
         inds = (iPt-1)*3+1:3*iPt;
         v = Xs(inds);
         XsXs = v*v';
-        rows((iPt-1)*9+1:iPt*9)=...
-            [inds(1) inds(1) inds(1) inds(2) inds(2) inds(2) inds(3) inds(3) inds(3)]';
-        cols((iPt-1)*9+1:iPt*9)=...
-            [inds(1) inds(2) inds(3) inds(1) inds(2) inds(3) inds(1) inds(2) inds(3)]';
-        Mloc=1/(8*pi*mu)*((aI*eye(3)+atau*XsXs)+Ls(iPt)*(eye(3)+XsXs));
-        vals((iPt-1)*9+1:iPt*9)=Mloc(:);
+        Mloc=1/(8*pi*mu)*(aI(iPt)*eye(3)+atau(iPt)*XsXs);
+        M(:,:,iPt)=Mloc;
     end
-    M = sparse(rows,cols,vals);
 end
