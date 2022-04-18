8fb0b378aee7a9f52279284869bacb3bfe6a81fe
RaulPPealez <raul.perez@uam.es>
2022-04-18 13:02:38 +0200

    Fix missing slash in install script

--- install.sh
+++ install.sh
@@ -6,6 +6,5 @@ if grep "SLENDER_PYTHON" ~/.bashrc; then echo "Slender body already present in b
 cat <<EOF >> ~/.bashrc
 export SLENDER_ROOT=$(pwd)
 export SLENDER_PYTHON=\$SLENDER_ROOT/Python
-export PYTHONPATH=${PYTHONPATH}:\$SLENDER_PYTHON:\$SLENDER_PYTHON/Dependencies:\$SLENDER_PYTHON/Dependencies/BatchedNBodyRPY:\$SLENDER_PYTHON/Dependencies/UAMMD_PSE_Python:\$SLENDER_PYTHON/Dependencies/NeighborSearch:\$SLENDER_PYTHON/cppmodules:\$SLENDER_ROOT/Fortran
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:\$SLENDER_ROOT/Fortran
+export PYTHONPATH=\${PYTHONPATH}:\$SLENDER_PYTHON:\$SLENDER_PYTHON/Dependencies:\$SLENDER_PYTHON/Dependencies/BatchedNBodyRPY:\$SLENDER_PYTHON/Dependencies/UAMMD_PSE_Python:\$SLENDER_PYTHON/Dependencies/NeighborSearch:\$SLENDER_PYTHON/cppmodules:\$SLENDER_ROOT/Fortran
 EOF
