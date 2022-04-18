65e6c12f39924318a2ec94f99b4d6ac817dc377a
RaulPPealez <raul.perez@uam.es>
2022-04-18 13:12:19 +0200

    Small changes to install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 set -e #End script if any command fails
-(cd Python && make)
+make -C Python
 #Check if slender is already present:
-if grep "SLENDER_PYTHON" ~/.bashrc; then echo "Slender body already present in bashrc"; exit 0; fi
+echo "Compilation was successful"
+
+if grep -q "SLENDER_PYTHON" ~/.bashrc; then echo "Slender body already present in bashrc"; exit 0; fi
 #If SLENDER_PYTHON is not present in bashrc append these lines to it
+echo "Appending paths to .bashrc"
 cat <<EOF >> ~/.bashrc
+# -------- ADDED BY SLENDERBODY INSTALLER ---------------
 export SLENDER_ROOT=$(pwd)
 export SLENDER_PYTHON=\$SLENDER_ROOT/Python
-export PYTHONPATH=\${PYTHONPATH}:\$SLENDER_PYTHON:\$SLENDER_PYTHON/Dependencies:\$SLENDER_PYTHON/Dependencies/BatchedNBodyRPY:\$SLENDER_PYTHON/Dependencies/UAMMD_PSE_Python:\$SLENDER_PYTHON/Dependencies/NeighborSearch:\$SLENDER_PYTHON/cppmodules:\$SLENDER_ROOT/Fortran
+export PYTHONPATH=\${PYTHONPATH}:\$SLENDER_PYTHON:\$SLENDER_PYTHON/Dependencies:\$SLENDER_PYTHON/Dependencies/BatchedNBodyRPY:\$SLENDER_PYTHON/Dependencies/UAMMD_PSE_Python:\$SLENDER_PYTHON/Dependencies/NeighborSearch:\$SLENDER_PYTHON/cppmodules
+# ----------------------------------------------------------
 EOF
