9226b23c45b669102839dfae579346434d58d18a
RaulPPealez <raul.perez@uam.es>
2022-04-19 17:33:18 +0200

    Fix compilation issue

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ cat <<EOF >> ~/.bashrc
 # -------- ADDED BY SLENDERBODY INSTALLER ---------------
 export SLENDER_ROOT=$(pwd)
 export SLENDER_PYTHON=\$SLENDER_ROOT/Python
-export PYTHONPATH=\${PYTHONPATH}:\$SLENDER_PYTHON:\$SLENDER_PYTHON/Dependencies:\$SLENDER_PYTHON/Dependencies/BatchedNBodyRPY:\$SLENDER_PYTHON/Dependencies/UAMMD_PSE_Python:\$SLENDER_PYTHON/Dependencies/NeighborSearch:\$SLENDER_PYTHON/cppmodules:\$SLENDER_ROOT/Fortran
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:\$SLENDER_ROOT/Fortran
+export PYTHONPATH=\${PYTHONPATH}:\$SLENDER_PYTHON:\$SLENDER_PYTHON/Dependencies:\$SLENDER_PYTHON/Dependencies/BatchedNBodyRPY:\$SLENDER_PYTHON/Dependencies/UAMMD_PSE_Python:\$SLENDER_PYTHON/Dependencies/NeighborSearch:\$SLENDER_PYTHON/cppmodules
 # ----------------------------------------------------------
 EOF
