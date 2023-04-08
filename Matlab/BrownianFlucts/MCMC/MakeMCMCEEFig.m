9d4237988069528136e2dacc441b248de086df83
Ondrej Maxian <ondrejmaxian@pop-os.localdomain>
2023-04-08 11:41:48 -0400

    MCMC figure

---
+++ Matlab/BrownianFlucts/MCMC/MakeMCMCEEFig.m
@@ -0,0 +1,58 @@
+names = ["SpecMCMCFreeConstKbT_N12_Lp1.mat" ...
+    "SpecMCMCFreeConstKbT_N24_Lp1.mat" "SpecMCMCFreeConstKbT_N36_Lp1.mat" ...
+    "SpecMCMCFreeConstKbT_N48_Lp1.mat" "FreeUnifMCMCkbT_Lp1.mat"];
+for iName = 1:length(names)
+load(names(iName))
+plotIndex=iName;
+nbins = 250;
+AllEndToEndDists = AllEndToQuarterDists;
+L = 0.25*L;
+histedges=(0:nbins)*L/nbins;
+NumPerBin = 1000/nbins;
+dshist = histedges(2)-histedges(1);
+histmps = (histedges(1:end-1)+histedges(2:end))/2;
+ChebHistCounts = zeros(nTrial,length(histedges)-1);
+nSaveSamples=sum(AllEndToEndDists(1,:));
+for iTrial=1:nTrial
+    if (NumPerBin > 1)
+        ChebHistCounts(iTrial,:) = sum(reshape(AllEndToEndDists(iTrial,:),NumPerBin,[]))/...
+            (nSaveSamples*dshist);
+    else
+        ChebHistCounts(iTrial,:) = AllEndToEndDists(iTrial,:)/(nSaveSamples*dshist);
+    end
+end
+if (plotIndex==length(names))
+h(length(h)+1)=plot(histmps/L,mean(L*ChebHistCounts),'k');
+else
+set(gca,'ColorOrderIndex',plotIndex)
+h(plotIndex)=plot(histmps/L,mean(L*ChebHistCounts));
+end
+hold on
+errorBarEvery=5;
+if (plotIndex==length(names))
+errorbar(histmps(plotIndex:errorBarEvery:end)/L,...
+    mean(L*ChebHistCounts(:,plotIndex:errorBarEvery:end)),...
+    2*std(L*ChebHistCounts(:,plotIndex:errorBarEvery:end))/sqrt(nTrial),'ko','LineWidth',2.0,...
+    'MarkerSize',1);
+else
+set(gca,'ColorOrderIndex',plotIndex)
+errorbar(histmps(plotIndex:errorBarEvery:end)/L,...
+    mean(L*ChebHistCounts(:,plotIndex:errorBarEvery:end)),...
+    2*std(L*ChebHistCounts(:,plotIndex:errorBarEvery:end))/sqrt(nTrial),'o','LineWidth',2.0,...
+    'MarkerSize',1);
+hold on
+end
+if (iName==length(names)-1 && L ==2)% Theory curve
+dr=1e-5;
+r = (0.5:1/dr)'*dr;
+G = zeros(length(r),1);
+for ell=1:3
+    G=G+1./(lpstar*(1-r)).^(3/2).*exp(-(ell-1/2)^2./(lpstar*(1-r))).*...
+        (4*((ell-1/2)./sqrt(lpstar*(1-r))).^2-2);
+end
+% Estimate integral of G, normalize to 1
+G=G.*r.^2;
+G = G/sum(G*dr);
+h(length(h)+1)=plot(r,G);
+end
+end
