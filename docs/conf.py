2d704f0058ff4998f7efc6d4d06d0bac07043a7e
Ondrej Maxian <om759@blob.cims.nyu.edu>
2023-06-19 17:53:14 -0400

    Update conf

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,8 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../Python/'))
-#sys.path.insert(0, os.path.abspath('../Python/cppmodules/'))
+sys.path.insert(0, os.path.abspath('../Python/cppmodules/'))
+sys.path.insert(0, os.path.abspath('../Python/Dependencies/'))
 
 # -- General configuration -----------------------------------------------------
 
