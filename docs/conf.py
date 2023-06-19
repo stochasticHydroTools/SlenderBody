23994562409430dd4f390b829fe89c539cfc79be
Ondrej Maxian <om759@blob.cims.nyu.edu>
2023-06-19 17:59:12 -0400

    Update conf

--- docs/conf.py
+++ docs/conf.py
@@ -35,8 +35,9 @@ sys.path.insert(0, os.path.abspath('../Python/Dependencies/'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.mathjax', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx_math_dollar','sphinx.ext.mathjax', 'sphinx.ext.napoleon']
 autodoc_member_order = 'bysource';
+autodoc_mock_imports = ['FiberCollectionC',' 
 toc_object_entries_show_parents  = 'hide';
 #autoclass_content = 'both';
 
