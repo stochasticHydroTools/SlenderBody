e22d388546da04baedd5e19936424cfafb7fd008
Ondrej Maxian <omaxian@crcfe01.crc.nd.edu>
2026-01-29 20:30:41 -0500

    Remove depracated stuff

--- Python/Dependencies/mykrypy/utils.py
+++ Python/Dependencies/mykrypy/utils.py
@@ -16,7 +16,7 @@ import scipy.linalg.blas as blas
 from scipy.sparse import isspmatrix
 
 # from scipy.sparse.linalg import LinearOperator, aslinearoperator
-from scipy.sparse.sputils import isintlike
+# from scipy.sparse.sputils import isintlike
 
 __all__ = [
     "ArgumentError",
@@ -1366,7 +1366,7 @@ class LinearOperator(object):
     """
 
     def __init__(self, shape, dtype, dot=None, dot_adj=None):
-        if len(shape) != 2 or not isintlike(shape[0]) or not isintlike(shape[1]):
+        if len(shape) != 2: #or not isintlike(shape[0]) or not isintlike(shape[1]):
             raise LinearOperatorError("shape must be (m,n) with m and n " "integer")
         self.shape = shape
         self.dtype = numpy.dtype(dtype)  # defaults to float64
@@ -1522,8 +1522,8 @@ class _PowerLinearOperator(LinearOperator):
             raise LinearOperatorError("LinearOperator expected as A")
         if A.shape[0] != A.shape[1]:
             raise LinearOperatorError("square LinearOperator expected as A")
-        if not isintlike(p):
-            raise LinearOperatorError("integer expected as p")
+        #if not isintlike(p):
+        #    raise LinearOperatorError("integer expected as p")
         self.args = (A, p)
         super(_PowerLinearOperator, self).__init__(
             A.shape, A.dtype, self._dot, self._dot_adj
