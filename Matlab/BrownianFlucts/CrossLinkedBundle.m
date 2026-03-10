7021251bb057e3cd5ac25bad45d1a7f1e0c76c05
Ondrej Maxian <omaxian@nd.edu>
2026-03-10 17:11:02 -0400

    Fluctuating fiber bundle

--- Matlab/BrownianFlucts/CrossLinkedBundle.m
+++ Matlab/BrownianFlucts/CrossLinkedBundle.m
@@ -1,15 +1,18 @@
-%function FluctClamped_AddDOFs(seed,ForceRt,N,dt,gtype)
-% Single fluctuating clamped filament
+% Fluctuating bundle of cross-linked filaments with Nlinks at arbitrary
+% locations
+% TODO: Figure out the inverse of K for multiple links. 
+% Understand source of instabilities when links are too close to the ends. 
 seed=1;
-N=16;
+Nx=12;
 dt=1e-3;
 gtype=1;
 addpath(genpath('../'))
+Nlinks = 2;
+LinkLocs = [0.25 0.75];
 %close all;
 rng(seed);
 nFib = 2;
-Nc_offgrid = 0; % Number of off-grid tangent vector constraints 
-Nog = N - Nc_offgrid;
+N = Nx - Nlinks; % Number of off-grid tangent vector constraints 
 L = 1;   % microns
 rtrue = 4e-3; % 4 nm radius
 eps = rtrue/L;
@@ -20,51 +23,70 @@ mu = 1;
 impcoeff = 1;
 makeMovie = 1;
 tf = 5;
-Tau0BC = [0;1;0];
-TrkLoc = L/2;
-XTrk=[0 0.1;TrkLoc TrkLoc;0 0];
-[sog,~,~] = chebpts(Nog, [0 L], gtype);
-Xs3=repmat(Tau0BC',nFib*N,1);
+Tau0 = [0;1;0];
+LinkLocs=reshape(LinkLocs,Nlinks,1);
+XLocs = zeros(3*Nlinks,nFib);
+XLocs(1:3:end,:)=repmat([0 0.1],Nlinks,1);
+XLocs(2:3:end,:)=repmat(LinkLocs,1,2);
+
+Xs3=repmat(Tau0',nFib*N,1);
 % Add rows for the constraints 
 [s,~,b] = chebpts(N,[0 L], gtype);
-sC=s;
-ChebToConstr = eye(N);
-ConstrToCheb = eye(N);
-Nx = N + 1;
-[sNp1,~,bNp1]=chebpts(Nx,[0 L],2);
-DNp1 = diffmat(Nx,[0 L],'chebkind2');
-RToNp1 = barymat(sNp1,s,b);
-RNp1ToN = barymat(s,sNp1,bNp1);
-IntDNp1 = pinv(DNp1);
-BMNp1 = stackMatrix(barymat(TrkLoc,sNp1,bNp1));
-% Construct matrix that gives X on the N+1 grid from (X_s, X_MP)
+[sNx,~,bNx]=chebpts(Nx,[0 L],2);
+DX = diffmat(Nx,[0 L],'chebkind2');
+RXToLocs = barymat(LinkLocs,sNx,bNx);
+RXToN = barymat(s,sNx,bNx);
+
+InvXonNp1Mat = [RXToN*DX; RXToLocs];
+if (~isempty(null(InvXonNp1Mat)))
+    error('Polynomial is not unique - switch discretization')
+end
+XonNp1Mat = InvXonNp1Mat^(-1);
+InvXonNp1Mat = stackMatrix(InvXonNp1Mat);
+XonNp1Mat = stackMatrix(XonNp1Mat);
 I=zeros(3*Nx,3);
 for iR=1:Nx
     I(3*(iR-1)+1:3*iR,1:3)=eye(3);   
 end
-XonNp1Mat = [(eye(3*Nx)-repmat(BMNp1,Nx,1))*...
-    stackMatrix(IntDNp1*RToNp1*ConstrToCheb) I];
-InvXonNp1Mat = [stackMatrix(ConstrToCheb \ RNp1ToN*DNp1); BMNp1];
+BMNp1 = stackMatrix(RXToLocs(1,:));
 
-% Bending energy matrix (2N+2 grid)
-[s2Np2, w2Np2, ~] = chebpts(2*Nx, [0 L], 2);
-W2Np2 = diag(w2Np2);
-R_Np1_To_2Np2 = barymat(s2Np2,sNp1,bNp1);
-WTilde_Np1 = stackMatrix((R_Np1_To_2Np2'*W2Np2*R_Np1_To_2Np2));
-WTilde_Np1_Inverse = WTilde_Np1^(-1);
-BendingEnergyMatrix_Np1 = Eb*stackMatrix((R_Np1_To_2Np2*DNp1^2)'*...
-    W2Np2*R_Np1_To_2Np2*DNp1^2);
-BendForceMat = -BendingEnergyMatrix_Np1;
-BendMatHalf_Np1 = real(BendingEnergyMatrix_Np1^(1/2));
+
+% Matrix that extracts the link velocities
+P = zeros(6*Nlinks,nFib*3*Nx);
+AvgMat = zeros(3*Nlinks,6*Nlinks);
+DiffMat = zeros(3*Nlinks,6*Nlinks);
+for d=1:3
+    for iLink=1:Nlinks
+        AvgMat(3*(iLink-1)+d,3*(iLink-1)+d:3*Nlinks:end)=1/2;
+        DiffMat(3*(iLink-1)+d,3*(iLink-1)+d:3*Nlinks:end)=1/2;
+        % Fiber 1 
+        P(3*(iLink-1)+d,3*N+3*(iLink-1)+d)=1;
+        % Fiber 2
+        P(3*Nlinks+3*(iLink-1)+d,3*Nx+3*N+3*(iLink-1)+d)=1;
+    end
+end
+DiffMat(:,3*Nlinks+1:end)=-1*DiffMat(:,3*Nlinks+1:end);
+LinkInverse = [AvgMat;DiffMat]*P*blkdiag(InvXonNp1Mat,InvXonNp1Mat);
+
+% Bending energy matrix (2Nx grid)
+[s2Nx, w2x, ~] = chebpts(2*Nx, [0 L], 2);
+W2Nx = diag(w2x);
+R_Nx_To_2Nx = barymat(s2Nx,sNx,bNx);
+WTilde_Nx = stackMatrix((R_Nx_To_2Nx'*W2Nx*R_Nx_To_2Nx));
+WTilde_Nx_Inverse = WTilde_Nx^(-1);
+BendingEnergyMatrix_Nx = Eb*stackMatrix((R_Nx_To_2Nx*DX^2)'*...
+    W2Nx*R_Nx_To_2Nx*DX^2);
+BendForceMat = -BendingEnergyMatrix_Nx;
+BendMatHalf = real(BendingEnergyMatrix_Nx^(1/2));
+BendMatAll = blkdiag(BendForceMat,BendForceMat);
+BendMatHalfAll = blkdiag(BendMatHalf,BendMatHalf);
 
 Xt = zeros(3*nFib*Nx,1);
 for iFib=1:nFib
-    Xt(3*Nx*(iFib-1)+1:3*Nx*iFib) = XonNp1Mat* [reshape(Xs3(N*(iFib-1)+1:N*iFib,:)',[],1);XTrk(:,iFib)];
+    Xt(3*Nx*(iFib-1)+1:3*Nx*iFib) = XonNp1Mat* [reshape(Xs3(N*(iFib-1)+1:N*iFib,:)',[],1);XLocs(:,iFib)];
 end
 saveEvery=max(1,floor(1e-2/dt+1e-10));
 MobConst = -log(eps^2)/(8*pi*mu);
-ConnectedMat = [stackMatrix([barymat(TrkLoc,sNp1,bNp1)]) -stackMatrix([barymat(TrkLoc,sNp1,bNp1)])];
-Constr=zeros(1,1);
 
 %% Initialization 
 stopcount=floor(tf/dt+1e-5);
@@ -73,7 +95,7 @@ Xpts=[];
 ee=[];
 Npl=100;
 [spl,~,~]=chebpts(Npl,[0 L]);
-RplNp1 = barymat(spl,sNp1,bNp1);
+RplNp1 = barymat(spl,sNx,bNx);
 if (makeMovie)
     close all;
     f=figure;
@@ -88,6 +110,7 @@ for count=0:stopcount
     if (mod(count,saveEvery)==0)
         %t
         PtsThisT = reshape(Xt,3,Nx*nFib)';
+        DOFs = blkdiag(InvXonNp1Mat(1:3:end,1:3:end),InvXonNp1Mat(1:3:end,1:3:end))*PtsThisT;
         if (makeMovie)
             clf;
             %nexttile
@@ -97,8 +120,10 @@ for count=0:stopcount
                     RplNp1*PtsThisT((iFib-1)*Nx+1:iFib*Nx,3));
                 hold on
             end
-            linkPts=[PtsThisT(9,:); PtsThisT(26,:)];
-            plot3(linkPts(:,1),linkPts(:,2),linkPts(:,3),':ko')
+            for pL = 1:Nlinks
+                linkPts=[DOFs(N+pL,:); DOFs(Nx+N+pL,:)];
+                plot3(linkPts(:,1),linkPts(:,2),linkPts(:,3),':ko')
+            end
             title(sprintf('$t=$ %2.1f',(frameNum-1)*saveEvery*dt),'Interpreter','latex')
             view(2)
             ylim([-1 2])
@@ -107,112 +132,132 @@ for count=0:stopcount
             movieframes(frameNum)=getframe(f);
         end
         Xpts=[Xpts;PtsThisT];
-        ee=[ee;norm(PtsThisT(end-Nx,:)-PtsThisT(end,:))];
-    end
-    ConsMat = Xt'*ConnectedMat'*ConnectedMat;
-    % Evolve system
-    MWsymTilde = zeros(nFib*3*Nx);
-    Ktilde = zeros(nFib*3*Nx);
-    KWithImp = zeros(nFib*3*Nx);
-    RandomVel = zeros(nFib*3*Nx,1);
-    BendForce = zeros(nFib*3*Nx,1);
-    TrkPts = zeros(3*nFib,1);
+        ee=[ee;norm(PtsThisT(end-Nx,:)-PtsThisT(1,:)); norm(PtsThisT(end,:)-PtsThisT(end-Nx+1,:))];
+    end  
+
+    % Matrices at time step n 
+    [KTogether,KTogetherInv] = KWithLink(nFib,N,Nx,Xt,XonNp1Mat,InvXonNp1Mat);
+    MWsym = zeros(nFib*3*Nx);
+    MWsymHalf = zeros(nFib*3*Nx);
     for iFib=1:nFib
         finds = 3*Nx*(iFib-1)+1:3*Nx*iFib;
         XsXTrk = reshape(InvXonNp1Mat*Xt(finds),3,Nx)';
-        XTrk = XsXTrk(end,:)';
-        TrkPts(3*iFib-2:3*iFib)=XTrk;
         Xs3 = XsXTrk(1:Nx-1,:);
-        MWsym = LocalDragMob(Xt(finds),DNp1,MobConst,WTilde_Np1_Inverse);
-        MWsymHalf = real(MWsym^(1/2));
-        % Obtain Brownian velocity
-        g = randn(3*Nx,1);
-        RandomVelBM = sqrt(2*kbT/dt)*MWsymHalf*g;
-        % Advance to midpoint
-        OmegaTilde = cross(Xs3,ChebToConstr*RNp1ToN*DNp1*reshape(RandomVelBM,3,[])');
-        Xstilde = rotateTau(Xs3,OmegaTilde(1:Nx-1,:),dt/2);
-        KtildeOne = KonNp1(Xstilde,XonNp1Mat,I);
-        Xtilde = XonNp1Mat*[reshape(Xstilde',[],1);XTrk];
-        MWsymTildeOne = LocalDragMob(Xtilde,DNp1,MobConst,WTilde_Np1_Inverse);
-        M_RFD = (MWsymTildeOne-MWsym)*(MWsym \ RandomVelBM);
-        RandomVelBE = sqrt(kbT)*...
-             MWsymTildeOne*BendMatHalf_Np1*randn(3*Nx,1);
-        RandomVel(finds) = RandomVelBM + M_RFD + RandomVelBE;
-        Ktilde(finds,finds)=KtildeOne;
-        KWithImp(finds,finds)=KtildeOne-impcoeff*dt*MWsymTildeOne*BendForceMat*KtildeOne;
-        MWsymTilde(finds,finds)=MWsymTildeOne;
-        BendForce(finds)=BendForceMat*Xt(finds);
+        MWsymOne = LocalDragMob(Xt(finds),DX,MobConst,WTilde_Nx_Inverse);
+        MWsymHalf = chol(MWsymOne);
+        MWsym(finds,finds)=MWsymOne;
+        MWsymHalf(finds,finds)=MWsymHalf;
     end
+    % Obtain Brownian velocity
+    g = randn(3*Nx*nFib,1);
+    RandomVelBM = sqrt(2*kbT/dt)*MWsymHalf*g;
+
+    % Advance to midpoint
+    OmegaTilde = KTogetherInv*RandomVelBM;
+    Xtilde = updateByRotate(nFib,N,Nx,Xt,OmegaTilde,XonNp1Mat,dt);
+    Ktilde = KWithLink(nFib,N,Nx,Xtilde,XonNp1Mat,InvXonNp1Mat);
+    MWsymTilde = zeros(nFib*3*Nx);
+    for iFib=1:nFib
+        finds = 3*Nx*(iFib-1)+1:3*Nx*iFib;
+        MWsymTildeOne = LocalDragMob(Xtilde(finds),DX,MobConst,WTilde_Nx_Inverse);
+        MWsymTilde(finds,finds)=MWsymTildeOne;
+    end
+    
+    % Solve at midpoint
+    M_RFD = (MWsymTilde-MWsym)*(MWsym \ RandomVelBM);
+    RandomVelBE = sqrt(kbT)*MWsymTilde*BendMatHalfAll*randn(3*Nx*nFib,1);
+    RandomVel = RandomVelBM + M_RFD + RandomVelBE;
+    KWithImp=Ktilde-impcoeff*dt*MWsymTilde*BendMatAll*Ktilde;
     U0 = zeros(3*Nx*nFib,1);
     Fext = zeros(3*Nx*nFib,1);
     MobK = pinv(Ktilde'*(MWsymTilde \ KWithImp));
-    MobC = ConsMat'*pinv(ConsMat*Ktilde*MobK*Ktilde'*ConsMat')*ConsMat;
-    alphaU = (MobK*Ktilde' - ...
-        MobK*Ktilde'*MobC*Ktilde*MobK*Ktilde')*...
-        (BendForce+ Fext + MWsymTilde \ (RandomVel + U0));
-    % Mat2 = [-MWsymTilde KWithImp zeros(3*Nx,nConstr); ...
-    %     Ktilde' zeros(3*Nx) Ktilde'*ConsMat'; ...
-    %     zeros(nConstr,3*Nx) ConsMat*Ktilde zeros(nConstr)];
-    % RHS2 = [MWsymTilde*(BendForceMat*Xt + Fext) + RandomVel+U0; ...
-    %     zeros(3*Nx+nConstr,1)];
-    % Sol2 = pinv(Mat2)*RHS2;
-    % Lambda = Sol2(1:3*Nx);
-    % alphaU1 = Sol2(3*Nx+1:6*Nx);
-    % Gamma = Sol2(6*Nx+1:end);
-    % if (max(abs(alphaU1-alphaU))>0.5)
-    %     keyboard
-    % end
+    alphaU = MobK*Ktilde'*(BendMatAll*Xt+ Fext + MWsymTilde \ (RandomVel + U0));
     % Evolve constants by rotating and translating the link
-    linkpts = [TrkPts(1:3)'; TrkPts(4:6)'];
-    r = linkpts(2,:)-linkpts(1,:);
-    u1u2 = [alphaU(3*Nx-2:3*Nx)'; alphaU(end-2:end)'];
-    xc = mean(linkpts);
-    uc = mean(u1u2);
-    xc=xc+dt*uc;
-    ud = u1u2(2,:)-u1u2(1,:);
-    OmLink=cross(r,ud)/norm(r)^2;
-    % Compute matrix representation you are using to get U and Omega
-    P = zeros(6,nFib*3*Nx);
-    for d=1:3
-        for iFib=1:nFib
-            P((iFib-1)*3+d,3*Nx*iFib-3+d)=1;
-        end
-    end
-    AvgMat = zeros(3,6);
-    for d=1:3
-        AvgMat(d,d:3:end)=1/2;
-    end
-    DiffMat = zeros(3,6);
-    for d=1:3
-        DiffMat(d,d:3:end)=1;
-    end
-    DiffMat(:,4:6)=-1*DiffMat(:,4:6);
-    OmLinkMat = eye(6);
-    OmLinkMat(4:6,4:6)=-CPMatrix(r)/norm(r)^2;
-    UcUDiff = [AvgMat;DiffMat]*P*alphaU;
-    TotalMatGetOmUcMat = OmLinkMat*[AvgMat;DiffMat]*P;
-    OmUc = TotalMatGetOmUcMat*alphaU;
-    linkHat = rotateTau(r,OmLink,dt);
-    linkpts_og=linkpts;
-    linkpts(1,:)=xc-linkHat/2;
-    linkpts(2,:)=xc+linkHat/2;
-    Xp1=0*Xt;
-    for iFib=1:nFib
-        finds = 3*Nx*(iFib-1)+1:3*Nx*iFib;
-        XsXTrk = reshape(InvXonNp1Mat*Xt(finds),3,Nx)';
-        Xs3 = XsXTrk(1:Nx-1,:);
-        Omega = reshape(alphaU((iFib-1)*3*Nx+1:iFib*3*Nx-3),3,N)';
-        newXs = rotateTau(Xs3,Omega,dt);
-        Xsp1 = reshape(newXs',[],1);
-        XTrk_p1 = linkpts(iFib,:)';
-        Xp1(finds) = XonNp1Mat*[Xsp1;XTrk_p1];
-    end
-    % Correct the constant
-    % Solve for minimum Omega s.t. you get back on constraint
-    ConstrErs(count+1) = abs(norm(ConnectedMat*Xp1)-0.1); 
+    Xp1 = updateByRotate(nFib,N,Nx,Xt,alphaU,XonNp1Mat,dt);
+    UActual = (Xp1-Xt)/dt;
+    ULinear = KTogether*alphaU;
     Xt=Xp1;
 end
 ConstrErs=ConstrErs(1:saveEvery:end);
 Totaltime=toc(tStart);
 %save(strcat('CDOFType',num2str(gtype),'_N',num2str(N),'_Dt',num2str(dt),'_Seed',num2str(seed),'.mat'))
 %end
+
+function [KTogether,KTogetherInv] = KWithLink(nFib,N,Nx,Xt,XonNp1Mat,InvXonNp1Mat)
+    DOFVelocity = zeros(nFib*3*Nx);
+    Nlinks = Nx - N;
+    TrkPts = zeros(3*nFib*Nlinks,1);
+    % The matrix for all the taus to evolve
+    for iFib=1:nFib
+        finds = 3*Nx*(iFib-1)+1:3*Nx*iFib;
+        XsXTrk = reshape(InvXonNp1Mat*Xt(finds),3,Nx)';
+        XTrk = XsXTrk(end-Nlinks+1:end,:)';
+        TrkPts(3*Nlinks*(iFib-1)+1:3*Nlinks*iFib)=XTrk(:);
+        Xs3 = XsXTrk(1:N,:);
+        TauCross = zeros(3*N);
+        for iR=1:N
+            inds = (iR-1)*3+1:iR*3;
+            TauCross(inds,inds)=CPMatrix(Xs3(iR,:));
+        end
+        FibOmegaInds = 3*N*(iFib-1)+(1:3*N);
+        FibTauInds = 3*Nx*(iFib-1)+(1:3*N);
+        DOFVelocity(FibTauInds,FibOmegaInds) =  -TauCross;
+        %InverseDOFTogether(3*Nx*(iFib-1)+1:3*Nx*(iFib-1)+3*N,3*Nx*(iFib-1)+1:3*Nx*(iFib-1)+3*N) = TauCross;
+    end
+    % The matrix for the links to evolve
+    for iLink=1:Nlinks
+        RLink = 1/2*(TrkPts((3*Nlinks+(iLink-1)*3+1:3*(Nlinks+iLink)))'-TrkPts((iLink-1)*3+1:3*iLink)');
+        CPMatR = CPMatrix(RLink);
+        % Velocity of each DOF should be = UL + RLink cross Omega
+        LinkUInds = nFib*3*N+((iLink-1)*3+1:3*iLink);
+        LinkOmInds = nFib*3*N+3*Nlinks+((iLink-1)*3+1:3*iLink);
+        Fib1Inds = 3*N+((iLink-1)*3+1:3*iLink);
+        Fib2Inds = 3*Nx+3*N+((iLink-1)*3+1:3*iLink);
+        DOFVelocity(Fib1Inds,LinkUInds)=eye(3);
+        DOFVelocity(Fib2Inds,LinkUInds)=eye(3);
+        DOFVelocity(Fib1Inds,LinkOmInds)=-CPMatR;
+        DOFVelocity(Fib2Inds,LinkOmInds)=CPMatR;
+        % Inverse operation Uc = (U1+U2)/2
+        % Compute matrix representation you are using to get U and Omega
+    end
+    KTogether = blkdiag(XonNp1Mat,XonNp1Mat)*DOFVelocity;
+    KTogetherInv = pinv(KTogether);
+end
+
+function XNew = updateByRotate(nFib,N,Nx,Xt,alphaU,XonNp1Mat,dt)
+    Nlinks = Nx - N;
+    TrkPts = zeros(3*nFib*Nlinks,1);
+    XsXtrk = zeros(3*Nx*nFib,1);
+    XNew = zeros(3*Nx*nFib,1);
+    % Update the tangent vectors
+    for iFib=1:nFib
+        finds = 3*Nx*(iFib-1)+1:3*Nx*iFib;
+        XsXTrk3 = reshape(XonNp1Mat \ Xt(finds),3,Nx)';
+        XTrk = XsXTrk3(end-Nlinks+1:end,:)';
+        TrkPts(3*Nlinks*(iFib-1)+1:3*Nlinks*iFib)=XTrk(:);
+        Xs3 = XsXTrk3(1:N,:);
+        Omega = reshape(alphaU((iFib-1)*3*N+1:iFib*3*N),3,N)';
+        newXs = rotateTau(Xs3,Omega,dt);
+        XsXtrk((iFib-1)*3*Nx+1:(iFib-1)*3*Nx+3*N)=reshape(newXs',[],1);
+    end
+    % Update the links
+    for iLink=1:Nlinks
+        LinkInds = 3*iLink-2:3*iLink;
+        linkpts = [TrkPts(LinkInds)'; TrkPts(3*Nlinks+LinkInds)']; 
+        r = linkpts(2,:)-linkpts(1,:);
+        xc = mean(linkpts);
+        uc = alphaU(6*N+LinkInds)';
+        OmLink = alphaU(6*N+3*Nlinks+LinkInds)';
+        xc=xc+dt*uc;
+        linkHat = rotateTau(r,-OmLink,dt);
+        XsXtrk(3*N+LinkInds)=xc-linkHat/2;
+        XsXtrk(3*Nx+3*N+LinkInds)=xc+linkHat/2;
+    end
+    for iFib=1:nFib
+        finds = 3*Nx*(iFib-1)+1:3*Nx*iFib;
+        XNew(finds) = XonNp1Mat*XsXtrk(finds);
+    end
+end
+
+
+
