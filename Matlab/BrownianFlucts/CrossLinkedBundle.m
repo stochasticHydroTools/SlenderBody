735144befd8b9212693bfb52fc7cdc5596f5521d
Ondrej Maxian <omaxian@nd.edu>
2026-03-11 09:39:26 -0400

    Still need to figure out discretization

--- Matlab/BrownianFlucts/CrossLinkedBundle.m
+++ Matlab/BrownianFlucts/CrossLinkedBundle.m
@@ -4,11 +4,11 @@
 % Understand source of instabilities when links are too close to the ends. 
 seed=1;
 Nx=12;
-dt=1e-3;
+dt=1e-4;
 gtype=1;
 addpath(genpath('../'))
 Nlinks = 2;
-LinkLocs = [0.25 0.75];
+LinkLocs = [0.25 0.8];
 %close all;
 rng(seed);
 nFib = 2;
@@ -22,7 +22,7 @@ Eb = lp*kbT; % pN*um^2 (Lp=17 um)
 mu = 1;
 impcoeff = 1;
 makeMovie = 1;
-tf = 5;
+tf = 1;
 Tau0 = [0;1;0];
 LinkLocs=reshape(LinkLocs,Nlinks,1);
 XLocs = zeros(3*Nlinks,nFib);
@@ -37,6 +37,11 @@ DX = diffmat(Nx,[0 L],'chebkind2');
 RXToLocs = barymat(LinkLocs,sNx,bNx);
 RXToN = barymat(s,sNx,bNx);
 
+% This is a delicate thing - not correct right now!
+% The way it is being done now, looking for the Nx degree polynomial that
+% goes through the tangent vectors and the points. Creates high order
+% instabilities (the bending forces are not "getting through" to the
+% tangents in this cases)
 InvXonNp1Mat = [RXToN*DX; RXToLocs];
 if (~isempty(null(InvXonNp1Mat)))
     error('Polynomial is not unique - switch discretization')
@@ -44,12 +49,18 @@ end
 XonNp1Mat = InvXonNp1Mat^(-1);
 InvXonNp1Mat = stackMatrix(InvXonNp1Mat);
 XonNp1Mat = stackMatrix(XonNp1Mat);
-I=zeros(3*Nx,3);
-for iR=1:Nx
-    I(3*(iR-1)+1:3*iR,1:3)=eye(3);   
-end
-BMNp1 = stackMatrix(RXToLocs(1,:));
 
+% Bending energy matrix (2Nx grid)
+[s2Nx, w2x, ~] = chebpts(2*Nx, [0 L], 2);
+W2Nx = diag(w2x);
+R_Nx_To_2Nx = barymat(s2Nx,sNx,bNx);
+WTilde_Nx = stackMatrix((R_Nx_To_2Nx'*W2Nx*R_Nx_To_2Nx));
+WTilde_Nx_Inverse = WTilde_Nx^(-1);
+BendingEnergyMatrix_Nx = Eb*stackMatrix(DX^2)'*WTilde_Nx*stackMatrix(DX^2);
+BendForceMat = -BendingEnergyMatrix_Nx;
+BendMatHalf = real(BendingEnergyMatrix_Nx^(1/2));
+BendMatAll = blkdiag(BendForceMat,BendForceMat);
+BendMatHalfAll = blkdiag(BendMatHalf,BendMatHalf);
 
 % Matrix that extracts the link velocities
 P = zeros(6*Nlinks,nFib*3*Nx);
@@ -66,26 +77,13 @@ for d=1:3
     end
 end
 DiffMat(:,3*Nlinks+1:end)=-1*DiffMat(:,3*Nlinks+1:end);
-LinkInverse = [AvgMat;DiffMat]*P*blkdiag(InvXonNp1Mat,InvXonNp1Mat);
-
-% Bending energy matrix (2Nx grid)
-[s2Nx, w2x, ~] = chebpts(2*Nx, [0 L], 2);
-W2Nx = diag(w2x);
-R_Nx_To_2Nx = barymat(s2Nx,sNx,bNx);
-WTilde_Nx = stackMatrix((R_Nx_To_2Nx'*W2Nx*R_Nx_To_2Nx));
-WTilde_Nx_Inverse = WTilde_Nx^(-1);
-BendingEnergyMatrix_Nx = Eb*stackMatrix((R_Nx_To_2Nx*DX^2)'*...
-    W2Nx*R_Nx_To_2Nx*DX^2);
-BendForceMat = -BendingEnergyMatrix_Nx;
-BendMatHalf = real(BendingEnergyMatrix_Nx^(1/2));
-BendMatAll = blkdiag(BendForceMat,BendForceMat);
-BendMatHalfAll = blkdiag(BendMatHalf,BendMatHalf);
+LinkInverse = [AvgMat;DiffMat]*P;
 
 Xt = zeros(3*nFib*Nx,1);
 for iFib=1:nFib
     Xt(3*Nx*(iFib-1)+1:3*Nx*iFib) = XonNp1Mat* [reshape(Xs3(N*(iFib-1)+1:N*iFib,:)',[],1);XLocs(:,iFib)];
 end
-saveEvery=max(1,floor(1e-2/dt+1e-10));
+saveEvery=1;%max(1,floor(1e-2/dt+1e-10));
 MobConst = -log(eps^2)/(8*pi*mu);
 
 %% Initialization 
@@ -136,7 +134,7 @@ for count=0:stopcount
     end  
 
     % Matrices at time step n 
-    [KTogether,KTogetherInv] = KWithLink(nFib,N,Nx,Xt,XonNp1Mat,InvXonNp1Mat);
+    [KTogether,KTogetherInv] = KWithLink(nFib,N,Nx,Xt,XonNp1Mat,InvXonNp1Mat,LinkInverse);
     MWsym = zeros(nFib*3*Nx);
     MWsymHalf = zeros(nFib*3*Nx);
     for iFib=1:nFib
@@ -155,7 +153,7 @@ for count=0:stopcount
     % Advance to midpoint
     OmegaTilde = KTogetherInv*RandomVelBM;
     Xtilde = updateByRotate(nFib,N,Nx,Xt,OmegaTilde,XonNp1Mat,dt);
-    Ktilde = KWithLink(nFib,N,Nx,Xtilde,XonNp1Mat,InvXonNp1Mat);
+    Ktilde = KWithLink(nFib,N,Nx,Xtilde,XonNp1Mat,InvXonNp1Mat,LinkInverse);
     MWsymTilde = zeros(nFib*3*Nx);
     for iFib=1:nFib
         finds = 3*Nx*(iFib-1)+1:3*Nx*iFib;
@@ -183,8 +181,9 @@ Totaltime=toc(tStart);
 %save(strcat('CDOFType',num2str(gtype),'_N',num2str(N),'_Dt',num2str(dt),'_Seed',num2str(seed),'.mat'))
 %end
 
-function [KTogether,KTogetherInv] = KWithLink(nFib,N,Nx,Xt,XonNp1Mat,InvXonNp1Mat)
+function [KTogether,KTogetherInv] = KWithLink(nFib,N,Nx,Xt,XonNp1Mat,InvXonNp1Mat,LinkInverse)
     DOFVelocity = zeros(nFib*3*Nx);
+    InvDOFVelocity = zeros(nFib*3*Nx);
     Nlinks = Nx - N;
     TrkPts = zeros(3*nFib*Nlinks,1);
     % The matrix for all the taus to evolve
@@ -202,9 +201,10 @@ function [KTogether,KTogetherInv] = KWithLink(nFib,N,Nx,Xt,XonNp1Mat,InvXonNp1Ma
         FibOmegaInds = 3*N*(iFib-1)+(1:3*N);
         FibTauInds = 3*Nx*(iFib-1)+(1:3*N);
         DOFVelocity(FibTauInds,FibOmegaInds) =  -TauCross;
-        %InverseDOFTogether(3*Nx*(iFib-1)+1:3*Nx*(iFib-1)+3*N,3*Nx*(iFib-1)+1:3*Nx*(iFib-1)+3*N) = TauCross;
+        InvDOFVelocity(FibOmegaInds,FibTauInds) = TauCross;
     end
     % The matrix for the links to evolve
+    OmegaInv = zeros(3*Nlinks);
     for iLink=1:Nlinks
         RLink = 1/2*(TrkPts((3*Nlinks+(iLink-1)*3+1:3*(Nlinks+iLink)))'-TrkPts((iLink-1)*3+1:3*iLink)');
         CPMatR = CPMatrix(RLink);
@@ -217,11 +217,12 @@ function [KTogether,KTogetherInv] = KWithLink(nFib,N,Nx,Xt,XonNp1Mat,InvXonNp1Ma
         DOFVelocity(Fib2Inds,LinkUInds)=eye(3);
         DOFVelocity(Fib1Inds,LinkOmInds)=-CPMatR;
         DOFVelocity(Fib2Inds,LinkOmInds)=CPMatR;
-        % Inverse operation Uc = (U1+U2)/2
-        % Compute matrix representation you are using to get U and Omega
+        OmegaInv((iLink-1)*3+1:3*iLink,(iLink-1)*3+1:3*iLink)=CPMatR/norm(RLink)^2;
     end
+    LinkInverse(3*Nlinks+1:end,:)=OmegaInv*LinkInverse(3*Nlinks+1:end,:);
+    InvDOFVelocity(6*N+1:end,:)=LinkInverse;
     KTogether = blkdiag(XonNp1Mat,XonNp1Mat)*DOFVelocity;
-    KTogetherInv = pinv(KTogether);
+    KTogetherInv = InvDOFVelocity*blkdiag(InvXonNp1Mat,InvXonNp1Mat);
 end
 
 function XNew = updateByRotate(nFib,N,Nx,Xt,alphaU,XonNp1Mat,dt)
