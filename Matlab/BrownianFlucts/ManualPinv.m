cc70f31e0f3af01c9c164f3e9d65d79e8386d337
Ondrej Maxian <omaxian@nd.edu>
2026-03-24 10:11:36 -0400

    Remove some files

--- Matlab/BrownianFlucts/ManualPinv.m
+++
@@ -1,9 +0,0 @@
-function PInvSP = ManualPinv(Mat,maxRank)
-    [U,S,V]=svd(Mat);
-    S=diag(S);
-    S(maxRank+1:end)=0;
-    S(S/S(1)<1e-10)=0;
-    pinvS = 1./S;
-    pinvS(S==0)=0;
-    PInvSP = V*diag(pinvS)*U';
-end
