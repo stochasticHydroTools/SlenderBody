33ab2266b0e6d0490d81950d9c88194861a00e9d
Ondrej Maxian <omaxian@nd.edu>
2026-07-14 14:08:06 -0400

    Fluct clamped on HPC

--- Matlab/BrownianFlucts/ConstrainedExamples/FluctClamped.m
+++ Matlab/BrownianFlucts/ConstrainedExamples/FluctClamped.m
@@ -1,10 +1,10 @@
-%function FluctClamped(seed,ForceRt,Nx,dt,clampL)
+function FluctClamped(seed,ForceRt,Nx,dt,clampL)
 % Single fluctuating clamped filament
-for seed=1:30
-ForceRt=10;
+%for seed=1:30
+%ForceRt=10;
 %seed=1;
-Nx=16;
-dt=1e-5;
+%Nx=16;
+%dt=1e-5;
 N = Nx-1;
 gtype='u';
 ConfineZ=0;
@@ -20,7 +20,7 @@ Eb = lp*kbT; % pN*um^2 (Lp=17 um)
 mu = 0.6;
 impcoeff = 1;
 makeMovie = 0;
-clampL=1;
+%clampL=1;
 tf = 50;
 Tau0BC = [0;1;0];
 %Tau0BC=rotate(Tau0BC',-70/180*pi*[0 0 1])';
