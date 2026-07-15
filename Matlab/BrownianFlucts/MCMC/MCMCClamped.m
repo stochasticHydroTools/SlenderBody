d84880e280d6cffd27efec2ddcdae9d8b8eae309
Ondrej Maxian <ondrejmaxian@Ondrejs-MacBook-Pro.local>
2026-07-15 17:04:44 -0400

    MCMC update

--- Matlab/BrownianFlucts/MCMC/MCMCClamped.m
+++ Matlab/BrownianFlucts/MCMC/MCMCClamped.m
@@ -1,24 +1,18 @@
-function MCMCClamped(Nx)
+function MCMCClamped(Nx,gtype)
 % Generate initial chain
 clampL=1;
 L = 1;
 kbT = 4.1e-3; % pN * um
 lp = 10*L;
 Eb = lp*kbT;
-nSamp = 1e7;
+nSamp = 1e8;
 nSaveSamples = 0.5*nSamp;
 nTrial = 10;
 %Nx = 8; % number tangent vectors
 N = Nx - 1;
-if (Nx==16)
-    TauConst=2e-3;
-elseif (Nx==24)
-    TauConst=2e-5;
-elseif (Nx==8)
-    TauConst=2e-1;
-end
+TauConst=2e-2*8/Nx;
 lpstar = Eb/kbT*1/L;
-gtype = 'u';
+%gtype = 1;
 Tau0BC = [0;1;0];
 TrkLoc=0;
 
@@ -28,7 +22,6 @@ try
 catch
     [s,~,b] = chebpts(N, [0 L], 1);
 end
-Xs3=repmat(Tau0BC',N,1);
 % Add rows for the constraints 
 sC=s;
 if (gtype==1)
@@ -47,7 +40,9 @@ else
     ChebToConstr = eye(N);
     ConstrToCheb = eye(N);
 end
-
+Xs3=repmat(Tau0BC',N,1);
+Xs3=Xs3+sqrt(((L - sC).*sC)./(L*lp)).*randn(N,3);
+Xs3=Xs3./sqrt(sum(Xs3.*Xs3,2));
 
 %%% Preliminary computations %%%
 [sNp1,~,bNp1]=chebpts(Nx,[0 L],2);
@@ -66,7 +61,8 @@ BendingEnergyMatrix_Np1 = Eb*stackMatrix((R_Np1_To_2Np2*DNp1^2)'*...
     W2Np2*R_Np1_To_2Np2*DNp1^2);
 
 % Propose a move around the state and evaluate its energy
-EPrev = 0;
+X = XonNp1Mat*reshape(Xs3',[],1);
+EPrev = 1/2*X'*BendingEnergyMatrix_Np1*X;
 nAcc=0;
 MeanTauSq = zeros(N,3,nTrial);
 
@@ -74,7 +70,7 @@ for iTrial=1:nTrial
 disp(strcat('New trial = ',num2str(iTrial)))
 tic
 for iSamp=1:nSamp
-    DTau = TauConst*sqrt(((L - sC).*sC)./(L*lp)).*randn(N,3);
+    DTau = TauConst.*randn(N,3);
     % Project off zero
     DTau(1,:)=0;
     if (clampL)
@@ -85,7 +81,8 @@ for iSamp=1:nSamp
     % Upsample and integrate, then upsample again
     X = XonNp1Mat*reshape(XsProp',[],1);
     Energy = 1/2*X'*BendingEnergyMatrix_Np1*X;
-    p_acc = exp(-Energy/kbT)/exp(-EPrev/kbT);
+    deltaE = (Energy-EPrev)/kbT;
+    p_acc = exp(-deltaE);
     r=rand;
     if (r < p_acc)
         EPrev=Energy;
@@ -99,7 +96,7 @@ for iSamp=1:nSamp
 end
 MeanTauSq(:,:,iTrial)=MeanTauSq(:,:,iTrial)/nSaveSamples;
 toc
-save(strcat('MCMCClamp_Nx',num2str(Nx),'_Lp',num2str(lpstar),'.mat'))
+save(strcat('MCMC',num2str(gtype),'_Clamp_Nx',num2str(Nx),'_Lp',num2str(lpstar),'.mat'))
 end
 end
 
